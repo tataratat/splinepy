1d57276509090d61f1fafe7fe7f2fe79f1836f7d
j042 <jaewooklee042@gmail.com>
2021-11-16 19:29:03 +0100

    load.py

---
+++ splinelibpy/load.py
@@ -0,0 +1,66 @@
+"""splinelibpy/splineplibpy/load.py
+
+Single function file containing `load_splines`.
+"""
+
+import os
+
+from splinelibpy._splinelibpy import Reader
+from splinelibpy.bspline import BSpline
+from splinelibpy.nurbs import NURBS
+from splinelibpy.utils import abs_fname
+
+def load_splines(fname):
+    """
+    Loads spline files of extension 
+      - `.iges`
+      - `.xml`
+      - `.itd`
+
+    Parameters
+    -----------
+    fname: str
+
+    Returns
+    --------
+    splines: list of BSpline or/and NURBS
+    """
+    fname = str(fname)
+    fname = abs_fname(fname)
+
+    sr = Reader()
+
+    ext = os.path.splitext(fname)[1]
+    
+    if ext == ".iges":
+        loaded_splines = sr.read_iges(fname)
+    elif ext == ".xml":
+        loaded_splines = sr.read_xml(fname)
+    elif ext == ".itd":
+        loaded_splines = sr.read_irit(fname)
+    else:
+        raise ImportError(
+            "We can only import < .iges | .xml | .itd > spline files"
+        )
+
+    splines = []
+    # Format s => [weights, degrees, knot_vectors, control_points]
+    for s in loaded_splines:
+        if s[0] is None:
+            # BSpline.
+            tmp_spline = BSpline()
+            tmp_spline.degrees = s[1]
+            tmp_spline.knot_vectors = s[2]
+            tmp_spline.control_points = s[3]
+            splines.append(tmp_spline)
+ 
+        else:
+            # Make nurbs
+            tmp_spline = NURBS()
+            tmp_spline.weights = s[0]
+            tmp_spline.degrees = s[1]
+            tmp_spline.knot_vectors = s[2]
+            tmp_spline.control_points = s[3]
+            splines.append(tmp_spline)
+
+    return splines
