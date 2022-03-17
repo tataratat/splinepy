9df61c13600892b70a3899ce07035574963a0faf
Jaewook Lee <jaewooklee042@gmail.com>
2022-03-17 08:50:09 +0100

    add spline ext

---
+++ src/splinelib_ext/nurbspl.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+// SplineLib
+#include <Sources/Splines/nurbs.hpp>
+
+template<int para_dim, int dim>
+class Nurbs : public splinelib::sources::splines::Nurbs<para_dim, dim> {
+public:
+  using Base_ = splinelib::sources::splines::Nurbs<para_dim, dim>;
+  using Coordinate_ = typename Base_::Coordinate_;
+  using Derivative_ = typename Base_::Derivative_;
+  using Knot_ = typename Base_::Knot_;
+  using ParameterSpace_ = typename Base_::ParameterSpace_;
+  using ParametricCoordinate_ = typename Base_::ParametricCoordinate_;
+  using WeightedVectorSpace_ = typename Base_::WeightedVectorSpace_;
+  using OutputInformation_ = splinelib::Tuple<
+      typename ParameterSpace_::OutputInformation_,
+      typename WeightedVectorSpace_::OutputInformation_
+  >;
+
+  // Some private ones. Here we make it public
+  //using BezierInformation_ =
+  //    typename Base_::ParameterSpace_::BezierInformation_;
+  //using BinomialRatios_ = typename Base_::ParameterSpace_::BinomialRatios_;
+  using Index_ = typename Base_::Base_::Index_;
+  using IndexLength_ = typename Index_::Length_;
+  using IndexValue_ = typename Index_::Value_;
+  using KnotRatios_ = typename Base_::ParameterSpace_::KnotRatios_;
+  using Knots_ = typename Base_::Base_::Knots_;
+  //using BinomialRatio_ = typename Base_::BinomialRatios_::value_type;
+  //using KnotRatio_ = typename KnotRatios_::value_type;
+  using HomogeneousBSpline_ = typename Base_::HomogeneousBSpline_;
+
+  // Constructor
+  using Base_::Base_;
+
+  // Computes (degree + 1) X ...
+  void BasisFunctionsAndIDs(ParametricCoordinate_ const &parametric_coordinate,
+                            double* basis_function_values,
+                            int* support_control_point_ids) const {
+
+    ParameterSpace_ const &parameter_space = *Base_::Base_::parameter_space_;
+
+    int i = 0;
+    double W = 0.0;
+    for (Index_ non_zero_basis_function{parameter_space.First()};
+         non_zero_basis_function != parameter_space.Behind();
+         ++non_zero_basis_function)
+    {
+
+      Index_ const &basis_function = (
+          parameter_space.FindFirstNonZeroBasisFunction(parametric_coordinate) +
+          non_zero_basis_function.GetIndex()
+      );
+
+      // general basis fn
+      const auto evaluated = parameter_space.EvaluateBasisFunction(
+          basis_function,
+          parametric_coordinate
+      );
+
+      // get `w` and add to `W`
+      const auto& support_id = basis_function.GetIndex1d().Get();
+      const double& w =
+          *Base_::weighted_vector_space_[support_id][dim - 1].Get();
+
+      const double N_times_w = evaluated * w;
+ 
+      W += N_times_w;
+      basis_function_values[i] = N_times_w; // not yet final
+      support_control_point_ids[i] = support_id;
+      i++;
+    }
+
+    // Loop and divide entries by W
+    int end = i;
+    double W_inv = 1 / W;
+    for (i = 0; i < end; i++) {
+      basis_function_values[i] *= W_inv;
+    }
+  }
+
+
+};
+
