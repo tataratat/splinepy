c5d19019682e5d27e6e6756c29154327573a5cf0
Jaewook Lee <jaewooklee042@gmail.com>
2022-04-11 15:19:04 +0200

    add raster_points

--- splinepy/utils.py
+++ splinepy/utils.py
@@ -119,3 +119,36 @@ def abs_fname(fname):
         fname = os.path.abspath(fname)
 
     return fname
+
+
+def raster_points(
+        bounds,
+        resolutions,
+):
+    """
+    Simple wraper of np.mgrid to extract raster points of desired bounds and
+    resolutions.
+
+    Parameters
+    -----------
+    bounds: (2, d) array-like
+      float
+    resolutions: (d,) array-like
+      int. It will be casted to int.
+
+    Returns
+    --------
+    raster_vertices: Vertices
+    """
+    if len(resolutions) != len(bounds[0]) == len(bounds[1]):
+        raise ValueError("Length of resolutions and bounds should match.")
+
+    points = "np.mgrid["
+    for i, (b0, b1) in enumerate(zip(bounds[0], bounds[1])):
+        points += f"{b0}:{b1}:{int(resolutions[i])}j,"
+    points += "]"
+
+    # Organize it nicely: 2D np.ndarray with shape (prod(resolutions), dim)
+    points = eval(points).T.reshape(-1, len(resolutions))
+
+    return points
