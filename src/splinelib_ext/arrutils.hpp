195e8c3375ee5921d6dd9bea64865dc288836121
Jaewook Lee <jaewooklee042@gmail.com>
2022-05-11 08:36:58 +0200

    try to fit into 79

--- src/splinelib_ext/arrutils.hpp
+++ src/splinelib_ext/arrutils.hpp
@@ -4,7 +4,10 @@
 #include <math.h>
 
 #include <Sources/Utilities/named_type.hpp>
-#include <Sources/VectorSpace/vector_space.hpp>
+#include <Sources/VectorSpaces/vector_space.hpp>
+#include <Sources/ParameterSpaces/parameter_space.hpp>
+  
+using namespace splinelib::sources;  
 
 /* elementwise subtraction */
 template<typename T, int dim>
@@ -20,8 +23,7 @@ inline void ew_minus(const std::array<T, dim>& arr1,
 /* elementwise subtraction overload for SplineLib Coordinate*/
 template<typename T, int dim>
 inline void ew_minus(
-    const
-        splinelib::sources::vector_spaces::VectorSpace<dim>::Coordinate_ arr1,
+    const vector_spaces::VectorSpace<dim>::Coordinate_ arr1,
     const std::array<T, dim>& arr2,
     std::array<T, dim>& out) {
   for (int i{0}; i < dim; i++) {
@@ -34,8 +36,7 @@ inline void ew_minus(
 template<typename T, int dim>
 inline void ew_minus(
     const std::array<T, dim>& arr1,
-    const
-        splinelib::sources::vector_spaces::VectorSpace<dim>::Coordinate_ arr2,
+    const vector_spaces::VectorSpace<dim>::Coordinate_ arr2,
     std::array<T, dim>& out) {
   for (int i{0}; i < dim; i++) {
     out[i] = arr1[i] - arr2[splinelib::Index{i}].Get();
@@ -47,8 +48,7 @@ inline void ew_minus(
 template<typename T, int dim>
 inline void ew_minus(
     const double* arr1,
-    const
-        splinelib::sources::vector_spaces::VectorSpace<dim>::Coordinate_ arr2,
+    const vector_spaces::VectorSpace<dim>::Coordinate_ arr2,
     std::array<T, dim>& out) {
 
   for (int i{0}; i < dim; i++) {
@@ -80,3 +80,39 @@ inline void norm2(std::array<T, dim>& in,
 
   out = sqrt(sqsum);
 }
+
+/* inplace operation for para coord clipping 
+ *
+ * Parameters
+ * -----------
+ * bounds: in
+ * para_coord: out
+ * clipped: out
+ *   -1 -> minimum clip
+ *    0 -> no clip
+ *    1 -> maximum clip
+ */
+template<typename T, int para_dim>
+inline void clip(
+    std::array<std::array<double, para_dim>, 2>& bounds,
+    parameter_spaces::ParameterSpace<para_dim>::ParameterCoordinate_& arr1,
+    std::array<int, para_dim> clipped) {
+
+  using PC =
+    typename parameter_spaces::ParameterSpaces<para_dim>::ParameterCoordinate_;
+  using SPC = PC::value_type;
+
+  for (int i{0}; i < para_dim; i++) {
+    // check max
+    if (arr1[i] > bounds[1][i]) {
+      clipped[i] = 1;
+      arr1[i] = SPC{bounds[1][i]};
+    } else if (arr1[i] < bounds[0][i]) {
+      clipped[i] = -1;
+      arr1[i] = SPC{bounds[0][i]};
+    } else {
+      clipped[i] = 0;
+    }
+  }
+}
+    
