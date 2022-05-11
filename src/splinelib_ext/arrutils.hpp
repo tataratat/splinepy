df4be446692e22a4bd78dcc01d10570603e42864
Jaewook Lee <jaewooklee042@gmail.com>
2022-05-11 11:05:57 +0200

    gauss_with_pivot rough draft

--- src/splinelib_ext/arrutils.hpp
+++ src/splinelib_ext/arrutils.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <array>
-#include <math.h>
+#include <cmath>
+#include <algorithm>
 
 #include <Sources/Utilities/named_type.hpp>
 #include <Sources/VectorSpaces/vector_space.hpp>
@@ -78,7 +79,7 @@ inline void norm2(std::array<T, dim>& in,
     sqsum += in[i] * in[i];
   }
 
-  out = sqrt(sqsum);
+  out = std::sqrt(sqsum);
 }
 
 /* inplace operation for para coord clipping 
@@ -115,4 +116,84 @@ inline void clip(
     }
   }
 }
-    
+
+
+/* Gauss elimination with partial pivoting to find x from (A x = b)
+ *
+ * Parameters
+ * -----------
+ * A: in
+ *   please excuse us, it will be modified
+ * b: in
+ *   please excuse us, it will be modified
+ * x: out
+ */
+template<typename T, int para_dim>
+inline void gauss_with_pivot(
+    std::array<std::array<double, para_dim>, para_dim>& A,
+    std::array<double, para_dim>& b,
+    std::array<int, para_dim>& skipmask,
+    std::array<double, para_dim>& x) {
+
+  int maxrow;
+  double maxval;
+  
+  // partial pivoting and forward reduction
+  for (int i{0}; i < para_dim; i++) {
+    // ignore clipped entries
+    if (skipmask[i] != 0) continue;
+
+    /* swap */
+    // go through the rows and compare magnitude and mark it if needed
+    maxrow = i;
+    maxval = std::abs(A[i][i]);
+    for (int j{i+1}; j < para_dim; j++) {
+      curmax = std::abs(A[j][i]);
+      if (curmax > maxval) {
+        maxval = curmax;
+        maxrow = j;
+      }
+    }
+    // swap if needed. max entry row goes to i-th row.
+    if (maxrow != i) {
+      // swap matrix entries
+      for (int j{0}; j < para_dim; j++) {
+        std::swap(A[maxrow][j], A[i][j]);
+      }
+      // swap row-vec entries
+      std::swap(b[maxrow], b[i]);
+    }
+    /* END swap */
+
+    /* forward reduction */
+    const double Aii = A[i][i];
+    for (int j{i+1}; j < para_dim; j++) {
+      const double redfac = A[j][i] / Aii;
+      // matrix reduction
+      for (int k{i+1}; k < para_dim; k++) {
+        A[j][k] -= A[i][k] * redfac;
+      }
+      A[j][i] = 0.;
+
+      // row-vec reduction0
+      b[j] -= b[i] * redfac;
+    }
+    /* END forward reduction */
+
+    // sneak in x initialization here.
+    x[i] = 0.;
+  }
+
+  // back substitution
+  double sum;
+  for (int i{para_dim-1}; i >= 0; i--) {
+    // ignore clipped entries
+    if (skipmask[i] != 0) continue;
+
+    sum = 0.;
+    for (int j{i+1}; j < para_dim; j++) {
+      sum += A[i][j] * x[j];
+    }
+    x[i] = (x[i] - sum) / A[i][i];
+  }
+}
