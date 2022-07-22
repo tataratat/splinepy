d243b341e511a438e63e4a060278b102c9034575
jzwar <zwar@ilsb.tuwien.ac.at>
2022-07-22 11:49:09 +0200

    Added Compositions between any two spline-types

--- splinepy/rational_bezier.py
+++ splinepy/rational_bezier.py
@@ -7,6 +7,7 @@ from splinepy import utils
 from splinepy._splinepy import *
 from splinepy._spline import Spline
 
+
 class RationalBezier(Spline):
 
     # Required Properties
@@ -35,7 +36,7 @@ class RationalBezier(Spline):
             control_points=control_points,
             weights=weights
         )
-        
+
     @property
     def weights(self,):
         """
@@ -76,7 +77,7 @@ class RationalBezier(Spline):
         weights = utils.make_c_contiguous(
             weights,
             dtype=np.float64
-        ).reshape(-1,1)
+        ).reshape(-1, 1)
 
         if self.control_points is not None:
             if self.control_points.shape[0] != weights.shape[0]:
@@ -86,7 +87,7 @@ class RationalBezier(Spline):
                 )
 
         self._weights = weights
-        
+
         logging.debug(f"Spline - {self.weights.shape[0]} Weights set.")
 
         self._check_and_update_c()
@@ -203,17 +204,17 @@ class RationalBezier(Spline):
         spline : New spline with updated types
         """
         if isinstance(factor, float):
-          return RationalBezier(
-              control_points=self._control_points * factor,
-              degrees=self._degrees,
-              weights=self._weights
-          )
+            return RationalBezier(
+                control_points=self._control_points * factor,
+                degrees=self._degrees,
+                weights=self._weights
+            )
 
         elif isinstance(factor, type(self)):
             if len(factor.degrees) == len(self._degrees):
                 if factor.dim == self.dim:
                     res_c_spl = self._c_spline.multiply_with_spline(
-                      factor._c_spline
+                        factor._c_spline
                     )
                 elif factor.dim == 1:
                     res_c_spl = self._c_spline.multiply_with_scalar_spline(
@@ -224,7 +225,7 @@ class RationalBezier(Spline):
                         self._c_spline
                     )
                 else:
-                  raise NotImplementedError()
+                    raise NotImplementedError()
 
                 # Copy the c spline to the python object
                 result = type(self)()
@@ -257,26 +258,27 @@ class RationalBezier(Spline):
           New spline that describes sum
         """
         if isinstance(summand, type(self)):
-          if (
-              summand.para_dim == self.para_dim
-              and summand.dim == self.dim
-          ):
-              # Calculate Spline sum
-              resulting_c_spline = self._c_spline.add_spline(
-                  summand._c_spline
-              )
+            if (
+                summand.para_dim == self.para_dim
+                and summand.dim == self.dim
+            ):
+                # Calculate Spline sum
+                resulting_c_spline = self._c_spline.add_spline(
+                    summand._c_spline
+                )
 
-              # Copy the c spline to the python object
-              result = type(self)()
-              result._c_spline = resulting_c_spline
-              result._update_p()
-              return result
+                # Copy the c spline to the python object
+                result = type(self)()
+                result._c_spline = resulting_c_spline
+                result._update_p()
+                return result
 
-          else :
-              raise TypeError("Dimension Mismatch.")
+            else:
+                raise TypeError("Dimension Mismatch.")
 
-        else :
-            raise TypeError("Sum must be formed between RationalBezier Splines")
+        else:
+            raise TypeError(
+                "Sum must be formed between RationalBezier Splines")
 
     def compose(self, inner_function):
         """
@@ -302,21 +304,21 @@ class RationalBezier(Spline):
                     "must match parametric dimension of outer spline"
                 )
             if inner_function.para_dim == 1:
-                res_c_spl = self._c_spline.compose_line(
+                res_c_spl = self._c_spline.compose_line_rr(
                     inner_function._c_spline
                 )
 
             elif inner_function.para_dim == 2:
-                res_c_spl = self._c_spline.compose_surface(
+                res_c_spl = self._c_spline.compose_surface_rr(
                     inner_function._c_spline
                 )
 
             elif inner_function.para_dim == 3:
-                res_c_spl = self._c_spline.compose_volume(
+                res_c_spl = self._c_spline.compose_volume_rr(
                     inner_function._c_spline
                 )
 
-            else :
+            else:
                 raise TypeError(
                     "Compositions with high"
                     " parametric dimensions not supported."
@@ -328,8 +330,37 @@ class RationalBezier(Spline):
             result._update_p()
             return result
 
-        else :
-            raise TypeError("Composisiton must be formed with RationalBezier Splines")
+        if inner_function.whatami.startswith("Bezier"):
+            if inner_function.para_dim == 1:
+                res_c_spl = self._c_spline.compose_line_rp(
+                    inner_function._c_spline
+                )
+
+            elif inner_function.para_dim == 2:
+                res_c_spl = self._c_spline.compose_surface_rp(
+                    inner_function._c_spline
+                )
+
+            elif inner_function.para_dim == 3:
+                res_c_spl = self._c_spline.compose_volume_rp(
+                    inner_function._c_spline
+                )
+
+            else:
+                raise TypeError(
+                    "Compositions with high"
+                    " parametric dimensions not supported."
+                )
+
+            # Copy the c spline to the python object
+            result = type(self)()
+            result._c_spline = res_c_spl
+            result._update_p()
+            return result
+
+        else:
+            raise TypeError(
+                "Composisiton must be formed with RationalBezier Splines")
 
     def copy(self):
         """
