42ac73cf39e65814868213b892c1482750e9d84a
jzwar <zwar@ilsb.tuwien.ac.at>
2022-07-25 19:15:06 +0200

    Draft of Bezier Extraction for NURBS
    Non functional but WIP

---
+++ cpp/splinepy/splines/type_identifier.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+
+
+namespace splinepy::splines {
+
+// Forward declarations
+template<int para_dim, int dim>
+class Nurbs;
+
+template<int para_dim, int dim>
+class BSpline;
+
+// Type Identifier
+template<typename SplineType>
+struct isNurbs {
+  constexpr static bool value = false;
+};
+
+template<int para_dim, int dim>
+struct isNurbs<Nurbs<para_dim, dim>> {
+  constexpr static bool value = true;
+};
+
+template<typename SplineType>
+constexpr bool isNurbs_v = isNurbs<SplineType>::value;
+
+
+template<typename SplineType>
+struct isBSpline {
+  constexpr static bool value = false;
+};
+
+template<int para_dim, int dim>
+struct isBSpline<BSpline<para_dim, dim>> {
+  constexpr static bool value = true;
+};
+
+template<typename SplineType>
+constexpr bool isBSpline_v = isBSpline<SplineType>::value;
+
+}
