cf8dccad9984bd620f7716a4e4c3374a3bfea7e8
jakhau <jakob.haupt@tuwien.ac.at>
2022-07-29 09:44:14 +0200

    Add test_remove_knots and test_reduce_order

--- tests/splinepyTest.py
+++ tests/splinepyTest.py
@@ -88,7 +88,7 @@ class TestSplinepy(unittest.TestCase):
         ]
 
     def test_constructor_degree_check(self):
-        """ test the raise Error for mismatch of degree and par_dim """
+        """ Test the raise Error for mismatch of degree and par_dim. """
         # not for Bezier
 
         self.b2P2D.update(degrees = [2,])
@@ -100,7 +100,7 @@ class TestSplinepy(unittest.TestCase):
             splinepy.NURBS(**self.n2P2D)
 
     def test_constructor_kv_nr_check(self):
-        """ test the raise Error for missing knot vectors """
+        """ Test the raise Error for missing knot vectors. """
         # not for Bezier
 
         self.b2P2D.update(knot_vectors = [[0, 0, 0, 0.5, 1, 1, 1]])
@@ -112,7 +112,7 @@ class TestSplinepy(unittest.TestCase):
             splinepy.NURBS(**self.n2P2D)
 
     def test_constructor_kv_length_check(self):
-        """ test the raise Error for length of knot vectors """
+        """ Test the raise Error for length of knot vectors. """
         # not for Bezier
 
         self.b2P2D.update(knot_vectors = [
@@ -132,7 +132,7 @@ class TestSplinepy(unittest.TestCase):
             splinepy.NURBS(**self.n2P2D)
 
     def test_constructor_cp_nr_check(self):
-        """ test the raise Error for invalid number of control points """
+        """ Test the raise Error for invalid number of control points. """
         # not for Bezier
 
         self.b2P2D.update(
@@ -168,8 +168,8 @@ class TestSplinepy(unittest.TestCase):
             splinepy.NURBS(**self.n2P2D)
 
     def test_basis_functions(self):
-        ''' test the correct calculation of the basis functions 
-        (.basis_functions()) '''
+        """ Test the correct calculation of the basis functions. 
+        (.basis_functions()) """
         # not for Bezier
 
         # reference solutions
@@ -215,8 +215,8 @@ class TestSplinepy(unittest.TestCase):
         )
 
     def test_evaluate(self):
-        ''' test the correct calculation of the spline coordinates 
-        in the physical space (.evaluate()) '''
+        """ Test the correct spline evaluation in the physical space.
+        (.evaluate()) """
 
         # reference solutions
         b_ref_evaluate = np.array(
@@ -264,8 +264,8 @@ class TestSplinepy(unittest.TestCase):
         )
 
     def test_derivative(self):
-        ''' test the correct calculation of the first derivative 
-        of the spline (.derivative()) '''
+        """ Test the correct calculation of the first derivative.  
+        (.derivative()) """
         # not for Bezier
 
         # reference solutions
@@ -303,8 +303,7 @@ class TestSplinepy(unittest.TestCase):
         )
 
     def test_insert_knot(self):
-        """ test the knot insertion function (.insert_knot()), 
-        (not for Bezier) """ 
+        """ Test the knot insertion function (.insert_knot()). """ 
         # not for Bezier
 
         self.b.insert_knots(0,[.2, .7])
@@ -369,13 +368,80 @@ class TestSplinepy(unittest.TestCase):
         )
 
     def test_remove_knot(self):
-        """ """
-        # needs to elevate and then reduce the degree in order to
-        # make sure there is a valid solution
-        # use .knot_vectors, basis_functions
+        """ Test the function .remove_knots. 
+        This test also depends on the function .insert_knots! """
+
+        # insert knots
+        self.b.insert_knots(0,[.2, .7,])
+        self.b.insert_knots(1,[.2, .5, .7,])
+
+        self.n.insert_knots(0,[.2, .5, .7,])
+        self.n.insert_knots(1,[.2, .5, .7,])
+
+        # remove knots
+        self.b.remove_knots(0,[.2, .7])
+        self.b.remove_knots(1,[.2, .5, .7,])
+
+        self.n.remove_knots(0,[.2, .5, .7,])
+        self.n.remove_knots(1,[.2, .5, .7,])
+
+        # reference solutions
+        b_ref_kv = [
+            [0, 0, 0, 0.5, 1, 1, 1], 
+            [0, 0, 0, 1, 1, 1]
+        ]
+        n_ref_kv = [
+            [0, 0, 0, 1, 1, 1], 
+            [0, 0, 1, 1]
+        ]
+        b_ref_basis_functions = np.array(
+            [[9.4128804e-01, 3.8615940e-02, 1.9602000e-04, 1.9015920e-02, 
+              7.8012000e-04, 3.9600000e-06, 9.6040000e-05, 3.9400000e-06, 
+              2.0000000e-08],
+             [2.4010000e-01, 9.8500000e-03, 5.0000000e-05, 4.8020000e-01, 
+              1.9700000e-02, 1.0000000e-04, 2.4010000e-01, 9.8500000e-03, 
+              5.0000000e-05],
+             [1.6200000e-02, 2.7540000e-01, 5.1840000e-01, 3.6000000e-03, 
+              6.1200000e-02, 1.1520000e-01, 2.0000000e-04, 3.4000000e-03, 
+              6.4000000e-03],
+             [7.2000000e-03, 5.0400000e-02, 3.2400000e-02, 3.3600000e-02, 
+              2.3520000e-01, 1.5120000e-01, 3.9200000e-02, 2.7440000e-01, 
+              1.7640000e-01],
+             [4.0000000e-06, 6.4000000e-05, 3.2000000e-05, 7.9200000e-04, 
+              1.2672000e-02, 6.3360000e-03, 3.9204000e-02, 6.2726400e-01, 
+              3.1363200e-01]] 
+        )
+        n_ref_basis_functions = np.array(
+            [[9.75958864e-01, 1.39415582e-02, 9.95774782e-05, 9.85817035e-03,
+                1.40823820e-04, 1.00583311e-06],
+              [4.92908517e-01, 7.04119101e-03, 5.02916557e-05, 4.92908517e-01,
+                7.04119101e-03, 5.02916557e-05],
+              [9.50089457e-03, 1.20926646e-01, 7.69572460e-01, 1.05565495e-03,
+                1.34362940e-02, 8.55080511e-02],
+              [1.32410262e-02, 7.49025551e-02, 2.11856419e-01, 3.08957277e-02,
+                1.74772629e-01, 4.94331644e-01],
+              [4.18891419e-03, 3.94934617e-03, 1.86173964e-03, 4.14702505e-01,
+                3.90985271e-01, 1.84312224e-01]]
+        )
+
+        # test knot_vectors
+        self.assertEqual(self.b.knot_vectors, b_ref_kv)
+        self.assertEqual(self.n.knot_vectors, n_ref_kv)
+        
+        # test basis functions
+        self.assertTrue(np.allclose(
+                            b_ref_basis_functions, 
+                            self.b.basis_functions(self.q2D)[0], 
+                            )
+        )
+        self.assertTrue(np.allclose(
+                            n_ref_basis_functions, 
+                            self.n.basis_functions(self.q2D)[0], 
+                            )
+        )
 
     def test_elevate_degree(self):
-        """ test the order elevation function (.elevate_degree()) """
+        """ Test the order elevation function (.elevate_degree()). """
         # not for Bezier
 
         self.b.elevate_degree(0)
@@ -441,10 +507,75 @@ class TestSplinepy(unittest.TestCase):
         )
 
     def test_reduce_degree(self):
-        """  """
-        # needs to elevate and then reduce the degree in order to
-        # make sure there is a valid solution
-        # use .degrees, .knot_vectors, basis_functions
+        """ Test the function .reduce_degree. 
+        This test also depends on the function .elevate_degree! """
+
+        # elevate order
+        self.b.elevate_degree(0)
+        self.n.elevate_degree(0)
+
+        # reduce order
+        self.b.reduce_degree(0)    
+        self.n.reduce_degree(0)
+
+        # reference solutions
+        b_ref_kv = [
+            [0, 0, 0, 0.5, 1, 1, 1], 
+            [0, 0, 0, 1, 1, 1]
+        ]
+        n_ref_kv = [
+            [0, 0, 0, 1, 1, 1], 
+            [0, 0, 1, 1]
+        ]
+        b_ref_basis_functions = np.array(
+            [[9.4128804e-01, 3.8615940e-02, 1.9602000e-04, 1.9015920e-02, 
+              7.8012000e-04, 3.9600000e-06, 9.6040000e-05, 3.9400000e-06, 
+              2.0000000e-08],
+             [2.4010000e-01, 9.8500000e-03, 5.0000000e-05, 4.8020000e-01, 
+              1.9700000e-02, 1.0000000e-04, 2.4010000e-01, 9.8500000e-03, 
+              5.0000000e-05],
+             [1.6200000e-02, 2.7540000e-01, 5.1840000e-01, 3.6000000e-03, 
+              6.1200000e-02, 1.1520000e-01, 2.0000000e-04, 3.4000000e-03, 
+              6.4000000e-03],
+             [7.2000000e-03, 5.0400000e-02, 3.2400000e-02, 3.3600000e-02, 
+              2.3520000e-01, 1.5120000e-01, 3.9200000e-02, 2.7440000e-01, 
+              1.7640000e-01],
+             [4.0000000e-06, 6.4000000e-05, 3.2000000e-05, 7.9200000e-04, 
+              1.2672000e-02, 6.3360000e-03, 3.9204000e-02, 6.2726400e-01, 
+              3.1363200e-01]] 
+        )
+        n_ref_basis_functions = np.array(
+            [[9.75958864e-01, 1.39415582e-02, 9.95774782e-05, 9.85817035e-03,
+                1.40823820e-04, 1.00583311e-06],
+              [4.92908517e-01, 7.04119101e-03, 5.02916557e-05, 4.92908517e-01,
+                7.04119101e-03, 5.02916557e-05],
+              [9.50089457e-03, 1.20926646e-01, 7.69572460e-01, 1.05565495e-03,
+                1.34362940e-02, 8.55080511e-02],
+              [1.32410262e-02, 7.49025551e-02, 2.11856419e-01, 3.08957277e-02,
+                1.74772629e-01, 4.94331644e-01],
+              [4.18891419e-03, 3.94934617e-03, 1.86173964e-03, 4.14702505e-01,
+                3.90985271e-01, 1.84312224e-01]]
+        )
+
+        # test degrees
+        self.assertTrue(np.allclose(self.b.degrees, [2, 2]))
+        self.assertTrue(np.allclose(self.n.degrees, [2, 1]))
+
+        # test knot_vectors
+        self.assertTrue(self.b.knot_vectors, b_ref_kv)
+        self.assertTrue(self.n.knot_vectors, n_ref_kv)
+
+        # test basis functions
+        self.assertTrue(np.allclose(
+                            b_ref_basis_functions, 
+                            self.b.basis_functions(self.q2D)[0], 
+                            )
+        )
+        self.assertTrue(np.allclose(
+                            n_ref_basis_functions, 
+                            self.n.basis_functions(self.q2D)[0], 
+                            )
+        )
 
 if __name__ == "__main__":
     unittest.main()
