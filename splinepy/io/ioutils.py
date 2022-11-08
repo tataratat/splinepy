74dd4e3a9e1fe6269f07e6724646c168751d1733
Jaewook Lee <jaewooklee042@gmail.com>
2022-11-08 16:02:38 +0100

    add abs_fname

--- splinepy/io/ioutils.py
+++ splinepy/io/ioutils.py
@@ -71,3 +71,26 @@ def form_lines(*args):
     return line_separated_str
 
 
+def abs_fname(fname):
+    """
+    Checks if fname is absolute. If not, returns abspath. Tilde safe.
+
+    Parameters
+    ----------
+    fname: str
+
+    Returns
+    --------
+    abs_fname: str
+      Maybe same to fname, maybe not.
+    """
+    if os.path.isabs(fname):
+        pass
+
+    elif "~" in fname:
+        fname = os.path.expanduser(fname)
+
+    else:
+        fname = os.path.abspath(fname)
+
+    return fname
