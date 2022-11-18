026e49972828c7de24b58dbcc6515add053883ad
Jaewook Lee <jaewooklee042@gmail.com>
2022-11-18 15:52:09 +0100

    add docs and NAME_TO_TYPE

--- splinepy/settings.py
+++ splinepy/settings.py
@@ -2,5 +2,26 @@
 Convenient global variables.
 """
 
+from splinepy import Bezier, RationalBezier, BSpline, NURBS
+
 TOLERANCE = 1e-11
+"""
+Default tolerance for any operation that involves floats
+"""
+
 NTHREADS = 1
+"""
+Default number of threads.
+"""
+
+NAME_TO_TYPE = dict(
+        Bezier=Bezier,
+        RationalBezier=RationalBezier,
+        BSpline=BSpline,
+        NURBS=NURBS,
+)
+"""
+String to Type conversion. Any function that may return different types should
+use this. If you have derived classes, replace this with your own conversion
+dict.
+"""
