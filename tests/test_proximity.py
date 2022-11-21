a364fb1dd5d21bfcae52418ad391e6ef8b0654c3
Jaewook Lee <jaewooklee042@gmail.com>
2022-11-21 10:45:59 +0100

    update and use common

--- tests/test_proximity.py
+++ tests/test_proximity.py
@@ -1,5 +1,3 @@
-import splinepy
-import numpy as np
 try:
     from . import common as c
 except BaseException:
@@ -14,43 +12,43 @@ class ProximityTest(c.unittest.TestCase):
         so excluded from test.
         """
         # make "iga book" bspline
-        bspline = splinepy.BSpline(**c.b2P2D)
+        bspline = c.splinepy.BSpline(**c.b2P2D)
 
         # form parametric queries
         # -> parametric space is [0, 1], so no further manipulation
-        para_q = np.random.random((10, bspline.para_dim))
+        para_q = c.np.random.random((10, bspline.para_dim))
 
         # form physical queries
         phys_q = bspline.evaluate(para_q)
 
         # proximity - single thread exe
-        prox_r = bspline.nearest_pcoord(
+        prox_r = bspline.proximities(
                 queries=phys_q,
-                kdt_resolutions=[10] * bspline.para_dim,
-                n_threads=1,
+                initial_guess_sample_resolutions=[10] * bspline.para_dim,
+                nthreads=1,
         )
 
-        assert np.allclose(para_q, prox_r), "WRONG `Spline.nearest_pcoord`"
+        assert c.np.allclose(para_q, prox_r[0]), "WRONG `Spline.nearest_pcoord`"
 
     def test_queries_inside_nurbs_initial_guess_with_kdt(self):
         # make half-half circle. also known as quarter circle
-        nurbs = splinepy.NURBS(**c.n2P2D)
+        nurbs = c.splinepy.NURBS(**c.n2P2D)
 
         # form parametric queries
         # -> parametric space is [0, 1], so no further manipulation
-        para_q = np.random.random((10, nurbs.para_dim))
+        para_q = c.np.random.random((10, nurbs.para_dim))
 
         # form physical queries
         phys_q = nurbs.evaluate(para_q)
 
         # proximity - single thread exe
-        prox_r = nurbs.nearest_pcoord(
+        prox_r = nurbs.proximities(
                 queries=phys_q,
-                kdt_resolutions=[10] * nurbs.para_dim,
-                n_threads=1,
+                initial_guess_sample_resolutions=[10] * nurbs.para_dim,
+                nthreads=1,
         )
 
-        assert np.allclose(para_q, prox_r), "WRONG `Spline.nearest_pcoord`"
+        assert c.np.allclose(para_q, prox_r[0]), "WRONG `Spline.nearest_pcoord`"
 
 
 if __name__ == "__main__":
