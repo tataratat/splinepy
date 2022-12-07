8f6729bc4eeaa7fbb83fcfa134fb6518b73a2f36
Jaewook Lee <jaewooklee042@gmail.com>
2022-12-07 12:06:30 +0100

    add splinepy explicit

---
+++ cpp/splinepy/explicit/splinepy/bezier_explicit.cpp
@@ -0,0 +1,115 @@
+#include <splinepy/splines/bezier.hpp>
+#include <splinepy/splines/rational_bezier.hpp>
+
+template class splinepy::splines::Bezier<1, 1>;
+template class splinepy::splines::Bezier<1, 2>;
+template class splinepy::splines::Bezier<1, 3>;
+#ifdef SPLINEPY_MORE
+template class splinepy::splines::Bezier<1, 4>;
+template class splinepy::splines::Bezier<1, 5>;
+template class splinepy::splines::Bezier<1, 6>;
+template class splinepy::splines::Bezier<1, 7>;
+template class splinepy::splines::Bezier<1, 8>;
+template class splinepy::splines::Bezier<1, 9>;
+template class splinepy::splines::Bezier<1, 10>;
+#endif
+template class splinepy::splines::Bezier<2, 1>;
+template class splinepy::splines::Bezier<2, 2>;
+template class splinepy::splines::Bezier<2, 3>;
+#ifdef SPLINEPY_MORE
+template class splinepy::splines::Bezier<2, 4>;
+template class splinepy::splines::Bezier<2, 5>;
+template class splinepy::splines::Bezier<2, 6>;
+template class splinepy::splines::Bezier<2, 7>;
+template class splinepy::splines::Bezier<2, 8>;
+template class splinepy::splines::Bezier<2, 9>;
+template class splinepy::splines::Bezier<2, 10>;
+#endif
+template class splinepy::splines::Bezier<3, 1>;
+template class splinepy::splines::Bezier<3, 2>;
+template class splinepy::splines::Bezier<3, 3>;
+#ifdef SPLINEPY_MORE
+template class splinepy::splines::Bezier<3, 4>;
+template class splinepy::splines::Bezier<3, 5>;
+template class splinepy::splines::Bezier<3, 6>;
+template class splinepy::splines::Bezier<3, 7>;
+template class splinepy::splines::Bezier<3, 8>;
+template class splinepy::splines::Bezier<3, 9>;
+template class splinepy::splines::Bezier<3, 10>;
+template class splinepy::splines::Bezier<4, 1>;
+template class splinepy::splines::Bezier<4, 2>;
+template class splinepy::splines::Bezier<4, 3>;
+template class splinepy::splines::Bezier<4, 4>;
+template class splinepy::splines::Bezier<4, 5>;
+template class splinepy::splines::Bezier<4, 6>;
+template class splinepy::splines::Bezier<4, 7>;
+template class splinepy::splines::Bezier<4, 8>;
+template class splinepy::splines::Bezier<4, 9>;
+template class splinepy::splines::Bezier<4, 10>;
+
+template class splinepy::splines::Bezier<5, 1>;
+template class splinepy::splines::Bezier<5, 2>;
+template class splinepy::splines::Bezier<5, 3>;
+template class splinepy::splines::Bezier<5, 4>;
+template class splinepy::splines::Bezier<5, 5>;
+template class splinepy::splines::Bezier<5, 6>;
+template class splinepy::splines::Bezier<5, 7>;
+template class splinepy::splines::Bezier<5, 8>;
+template class splinepy::splines::Bezier<5, 9>;
+template class splinepy::splines::Bezier<5, 10>;
+
+template class splinepy::splines::Bezier<6, 1>;
+template class splinepy::splines::Bezier<6, 2>;
+template class splinepy::splines::Bezier<6, 3>;
+template class splinepy::splines::Bezier<6, 4>;
+template class splinepy::splines::Bezier<6, 5>;
+template class splinepy::splines::Bezier<6, 6>;
+template class splinepy::splines::Bezier<6, 7>;
+template class splinepy::splines::Bezier<6, 8>;
+template class splinepy::splines::Bezier<6, 9>;
+template class splinepy::splines::Bezier<6, 10>;
+
+template class splinepy::splines::Bezier<7, 1>;
+template class splinepy::splines::Bezier<7, 2>;
+template class splinepy::splines::Bezier<7, 3>;
+template class splinepy::splines::Bezier<7, 4>;
+template class splinepy::splines::Bezier<7, 5>;
+template class splinepy::splines::Bezier<7, 6>;
+template class splinepy::splines::Bezier<7, 7>;
+template class splinepy::splines::Bezier<7, 8>;
+template class splinepy::splines::Bezier<7, 9>;
+template class splinepy::splines::Bezier<7, 10>;
+
+template class splinepy::splines::Bezier<8, 1>;
+template class splinepy::splines::Bezier<8, 2>;
+template class splinepy::splines::Bezier<8, 3>;
+template class splinepy::splines::Bezier<8, 4>;
+template class splinepy::splines::Bezier<8, 5>;
+template class splinepy::splines::Bezier<8, 6>;
+template class splinepy::splines::Bezier<8, 7>;
+template class splinepy::splines::Bezier<8, 8>;
+template class splinepy::splines::Bezier<8, 9>;
+template class splinepy::splines::Bezier<8, 10>;
+
+template class splinepy::splines::Bezier<9, 1>;
+template class splinepy::splines::Bezier<9, 2>;
+template class splinepy::splines::Bezier<9, 3>;
+template class splinepy::splines::Bezier<9, 4>;
+template class splinepy::splines::Bezier<9, 5>;
+template class splinepy::splines::Bezier<9, 6>;
+template class splinepy::splines::Bezier<9, 7>;
+template class splinepy::splines::Bezier<9, 8>;
+template class splinepy::splines::Bezier<9, 9>;
+template class splinepy::splines::Bezier<9, 10>;
+
+template class splinepy::splines::Bezier<10, 1>;
+template class splinepy::splines::Bezier<10, 2>;
+template class splinepy::splines::Bezier<10, 3>;
+template class splinepy::splines::Bezier<10, 4>;
+template class splinepy::splines::Bezier<10, 5>;
+template class splinepy::splines::Bezier<10, 6>;
+template class splinepy::splines::Bezier<10, 7>;
+template class splinepy::splines::Bezier<10, 8>;
+template class splinepy::splines::Bezier<10, 9>;
+template class splinepy::splines::Bezier<10, 10>;
+#endif
---
+++ cpp/splinepy/explicit/splinepy/nurbs_explicit.cpp
@@ -0,0 +1,114 @@
+#include <splinepy/splines/nurbs.hpp>
+
+template class splinepy::splines::Nurbs<1, 1>;
+template class splinepy::splines::Nurbs<1, 2>;
+template class splinepy::splines::Nurbs<1, 3>;
+#ifdef SPLINEPY_MORE
+template class splinepy::splines::Nurbs<1, 4>;
+template class splinepy::splines::Nurbs<1, 5>;
+template class splinepy::splines::Nurbs<1, 6>;
+template class splinepy::splines::Nurbs<1, 7>;
+template class splinepy::splines::Nurbs<1, 8>;
+template class splinepy::splines::Nurbs<1, 9>;
+template class splinepy::splines::Nurbs<1, 10>;
+#endif
+template class splinepy::splines::Nurbs<2, 1>;
+template class splinepy::splines::Nurbs<2, 2>;
+template class splinepy::splines::Nurbs<2, 3>;
+#ifdef SPLINEPY_MORE
+template class splinepy::splines::Nurbs<2, 4>;
+template class splinepy::splines::Nurbs<2, 5>;
+template class splinepy::splines::Nurbs<2, 6>;
+template class splinepy::splines::Nurbs<2, 7>;
+template class splinepy::splines::Nurbs<2, 8>;
+template class splinepy::splines::Nurbs<2, 9>;
+template class splinepy::splines::Nurbs<2, 10>;
+#endif
+template class splinepy::splines::Nurbs<3, 1>;
+template class splinepy::splines::Nurbs<3, 2>;
+template class splinepy::splines::Nurbs<3, 3>;
+#ifdef SPLINEPY_MORE
+template class splinepy::splines::Nurbs<3, 4>;
+template class splinepy::splines::Nurbs<3, 5>;
+template class splinepy::splines::Nurbs<3, 6>;
+template class splinepy::splines::Nurbs<3, 7>;
+template class splinepy::splines::Nurbs<3, 8>;
+template class splinepy::splines::Nurbs<3, 9>;
+template class splinepy::splines::Nurbs<3, 10>;
+template class splinepy::splines::Nurbs<4, 1>;
+template class splinepy::splines::Nurbs<4, 2>;
+template class splinepy::splines::Nurbs<4, 3>;
+template class splinepy::splines::Nurbs<4, 4>;
+template class splinepy::splines::Nurbs<4, 5>;
+template class splinepy::splines::Nurbs<4, 6>;
+template class splinepy::splines::Nurbs<4, 7>;
+template class splinepy::splines::Nurbs<4, 8>;
+template class splinepy::splines::Nurbs<4, 9>;
+template class splinepy::splines::Nurbs<4, 10>;
+
+template class splinepy::splines::Nurbs<5, 1>;
+template class splinepy::splines::Nurbs<5, 2>;
+template class splinepy::splines::Nurbs<5, 3>;
+template class splinepy::splines::Nurbs<5, 4>;
+template class splinepy::splines::Nurbs<5, 5>;
+template class splinepy::splines::Nurbs<5, 6>;
+template class splinepy::splines::Nurbs<5, 7>;
+template class splinepy::splines::Nurbs<5, 8>;
+template class splinepy::splines::Nurbs<5, 9>;
+template class splinepy::splines::Nurbs<5, 10>;
+
+template class splinepy::splines::Nurbs<6, 1>;
+template class splinepy::splines::Nurbs<6, 2>;
+template class splinepy::splines::Nurbs<6, 3>;
+template class splinepy::splines::Nurbs<6, 4>;
+template class splinepy::splines::Nurbs<6, 5>;
+template class splinepy::splines::Nurbs<6, 6>;
+template class splinepy::splines::Nurbs<6, 7>;
+template class splinepy::splines::Nurbs<6, 8>;
+template class splinepy::splines::Nurbs<6, 9>;
+template class splinepy::splines::Nurbs<6, 10>;
+
+template class splinepy::splines::Nurbs<7, 1>;
+template class splinepy::splines::Nurbs<7, 2>;
+template class splinepy::splines::Nurbs<7, 3>;
+template class splinepy::splines::Nurbs<7, 4>;
+template class splinepy::splines::Nurbs<7, 5>;
+template class splinepy::splines::Nurbs<7, 6>;
+template class splinepy::splines::Nurbs<7, 7>;
+template class splinepy::splines::Nurbs<7, 8>;
+template class splinepy::splines::Nurbs<7, 9>;
+template class splinepy::splines::Nurbs<7, 10>;
+
+template class splinepy::splines::Nurbs<8, 1>;
+template class splinepy::splines::Nurbs<8, 2>;
+template class splinepy::splines::Nurbs<8, 3>;
+template class splinepy::splines::Nurbs<8, 4>;
+template class splinepy::splines::Nurbs<8, 5>;
+template class splinepy::splines::Nurbs<8, 6>;
+template class splinepy::splines::Nurbs<8, 7>;
+template class splinepy::splines::Nurbs<8, 8>;
+template class splinepy::splines::Nurbs<8, 9>;
+template class splinepy::splines::Nurbs<8, 10>;
+
+template class splinepy::splines::Nurbs<9, 1>;
+template class splinepy::splines::Nurbs<9, 2>;
+template class splinepy::splines::Nurbs<9, 3>;
+template class splinepy::splines::Nurbs<9, 4>;
+template class splinepy::splines::Nurbs<9, 5>;
+template class splinepy::splines::Nurbs<9, 6>;
+template class splinepy::splines::Nurbs<9, 7>;
+template class splinepy::splines::Nurbs<9, 8>;
+template class splinepy::splines::Nurbs<9, 9>;
+template class splinepy::splines::Nurbs<9, 10>;
+
+template class splinepy::splines::Nurbs<10, 1>;
+template class splinepy::splines::Nurbs<10, 2>;
+template class splinepy::splines::Nurbs<10, 3>;
+template class splinepy::splines::Nurbs<10, 4>;
+template class splinepy::splines::Nurbs<10, 5>;
+template class splinepy::splines::Nurbs<10, 6>;
+template class splinepy::splines::Nurbs<10, 7>;
+template class splinepy::splines::Nurbs<10, 8>;
+template class splinepy::splines::Nurbs<10, 9>;
+template class splinepy::splines::Nurbs<10, 10>;
+#endif
