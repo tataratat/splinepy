c14c9206a1af7ad33fae7ca19c6fac71c97b4612
jzwar <zwar@ilsb.tuwien.ac.at>
2022-12-11 19:01:33 +0100

    Order alphabetically

--- splinepy/__init__.py
+++ splinepy/__init__.py
@@ -1,21 +1,23 @@
 from splinepy._version import __version__
 from splinepy import splinepy_core
 
-from splinepy import spline
 from splinepy import bezier
-from splinepy import rational_bezier
 from splinepy import bspline
-from splinepy import nurbs
-from splinepy import utils
-from splinepy import load
 from splinepy import io
+from splinepy import load
+from splinepy import multipatch
+from splinepy import nurbs
+from splinepy import rational_bezier
+from splinepy import spline
 from splinepy import settings
+from splinepy import utils
 
-from splinepy.spline import Spline
 from splinepy.bezier import Bezier
-from splinepy.rational_bezier import RationalBezier
 from splinepy.bspline import BSpline
+from splinepy.multipatch import Multipatch
 from splinepy.nurbs import NURBS
+from splinepy.rational_bezier import RationalBezier
+from splinepy.spline import Spline
 from splinepy.load import (load_splines, load_solution)
 
 # set NAME_TO_TYPE
@@ -26,21 +28,23 @@ utils.log.configure()
 
 __all__ = [
         "__version__",
-        "splinepy_core",
-        "spline",
         "bezier",
-        "rational_bezier",
         "bspline",
-        "nurbs",
-        "load",
-        "utils",
         "io",
+        "load",
+        "multipatch",
+        "nurbs",
+        "rational_bezier",
+        "spline",
+        "splinepy_core",
+        "utils",
         "settings",
-        "Spline",
         "Bezier",
-        "RationalBezier",
         "BSpline",
+        "Multipatch",
         "NURBS",
+        "RationalBezier",
+        "Spline",
         "load_splines",
         "load_solution",
 ]
