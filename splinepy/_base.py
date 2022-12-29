a15e0112c513b034fda44d15c32cf8d94779b834
Jaewook Lee <jaewooklee042@gmail.com>
2022-12-29 10:15:22 +0100

    __init_subclss__

--- splinepy/_base.py
+++ splinepy/_base.py
@@ -8,11 +8,14 @@ from splinepy.utils import log
 class SplinepyBase:
     __slots__ = ()
 
-    def __new__(cls, *args, **kwargs):
+    def __init_subclass__(cls, *args, **kwargs):
         """
         Add logger shortcut.
         """
-        cls._logi = log.prepend_log(cls.__qualname__, log.info)
-        cls._logd = log.prepend_log(cls.__qualname__, log.debug)
-        cls._logw = log.prepend_log(cls.__qualname__, log.warning)
+        super().__init_subclass__(*args, **kwargs)
+        cls._logi = log.prepend_log("<" + cls.__qualname__ + ">", log.info)
+        cls._logd = log.prepend_log("<" + cls.__qualname__ + ">", log.debug)
+        cls._logw = log.prepend_log(
+            "<" + cls.__qualname__ + ">", log.warning
+        )
         return super().__new__(cls)
