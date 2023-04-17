35db282a0f5250d0d0bb9b37f748f253029f2c7c
Jaewook Lee <jaewooklee042@gmail.com>
2023-04-17 21:17:32 +0200

    test entries of different len

--- tests/test_cartesian.py
+++ tests/test_cartesian.py
@@ -11,33 +11,41 @@ class CartesianProductTest(c.unittest.TestCase):
         """
         Test cartesian product test
         """
-        # define some array
-        a = c.np.array([0, 0.5, 1])
+        # define arrays for cartesian product
+        # make different len, but not too long so that test
+        # doesn't take too long.
+        max_dim = 10
+        pool = c.np.array([0.0, 1.1, 4.4, 8.8, 9.6])
+        queries = [pool[: c.np.random.randint(2, 5)] for _ in range(max_dim)]
+
+        # in case they are all the same, let's change two entries
+        queries[-1] = pool
+        queries[-2] = pool[:3]
 
         def ref(arrays, reverse):
             """create reference answers using itertools.product"""
-
-            prod = c.np.array(list(product(*arrays)))
             if reverse:
+                prod = c.np.array(list(product(*arrays[::-1])))
                 return prod[:, ::-1]
             else:
+                prod = c.np.array(list(product(*arrays)))
                 return prod
 
         # test cartesian product
-        for d in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10):
-            in_arr = [a] * d
+        for i in range(1, max_dim + 1):
+            in_arr = queries[:i]
             assert c.np.allclose(
                 ref(in_arr, False),
                 c.splinepy.utils.data.cartesian_product(in_arr, False),
-            ), f"{d}-dim failed"
+            ), f"{i}-dim failed with NO reverse"
 
         # test reversed dim - what's often used in lib
-        for d in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10):
-            in_arr = [a] * d
+        for i in range(1, max_dim + 1):
+            in_arr = queries[:i]
             assert c.np.allclose(
                 ref(in_arr, True),
                 c.splinepy.utils.data.cartesian_product(in_arr, True),
-            ), f"{d}-dim failed"
+            ), f"{i}-dim failed with reverse"
 
 
 if __name__ == "__main__":
