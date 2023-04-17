77ca1bc0835f262107f9a2af67b5be1a8600af89
Jaewook Lee <jaewooklee042@gmail.com>
2023-04-17 12:40:16 +0200

    add cartesian prod

--- splinepy/utils/data.py
+++ splinepy/utils/data.py
@@ -2,6 +2,7 @@
 
 Helps helpee to manage data. Some useful data structures.
 """
+from itertools import accumulate, chain, repeat
 
 import numpy as np
 
@@ -261,3 +262,55 @@ def enforce_contiguous_values(dict_):
         dict_with_contiguous[key] = value
 
     return dict_with_contiguous
+
+
+def cartesian_product(arrays, reverse=True):
+    """
+    Fast Cartesian product.
+    Adapted from stackoverflow entry:
+    https://stackoverflow.com/questions/11144513/
+    cartesian-product-of-x-and-y-array-points-into-single-array-of-2d-points/
+    49445693#49445693
+    Answer from `Paul Panzer`.
+
+    In splinepy, we use the ordering, where lowest dimension changes fastest.
+    Thus, a reverse option.
+
+    Parameters
+    ----------
+    arrays: tuple or list
+      tuple or list of array-like
+    reverse: bool
+      Default is True. Reverses order of fastest iterating dimension.
+      If False, highest indexed dimension iterates fastest.
+
+    Returns
+    -------
+    cartesian: (n, len(arrays)) np.ndarray
+    """
+    n_arr = len(arrays)
+
+    shape = *map(len, arrays), n_arr
+    dtype = np.result_type(*arrays)
+    cartesian = np.empty(shape, dtype=dtype)
+
+    # for reverse, use view of reverse ordered arrays
+    # plus, view of reverse ordered cartesian
+    if reverse:
+        arrays = arrays[::-1]
+        _cartesian = cartesian[..., ::-1]
+    else:
+        _cartesian = cartesian
+
+    dim_reducing_views = (
+        *accumulate(
+            chain((_cartesian,), repeat(0, n_arr - 1)), np.ndarray.__getitem__
+        ),
+    )
+    idx = slice(None), *repeat(None, n_arr - 1)
+    for i in range(n_arr - 1, 0, -1):
+        dim_reducing_views[i][..., i] = arrays[i][idx[: n_arr - i]]
+        dim_reducing_views[i - 1][1:] = dim_reducing_views[i]
+    _cartesian[..., 0] = arrays[0][idx]
+
+    return cartesian.reshape(-1, n_arr)
