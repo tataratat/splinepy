33b0ba919f370e9bd2b6d755251d078028c214a1
Jaewook Lee <jaewooklee042@gmail.com>
2023-04-18 21:56:04 +0200

    init/* only inits
    all pybind definitions are implemented in the same file as
    cpp implementations

--- cpp/splinepy/py/init/fitting.cpp
+++ cpp/splinepy/py/init/fitting.cpp
@@ -2,39 +2,6 @@
 
 namespace splinepy::py::init {
 
-void init_fitting(py::module_& m) {
-  // Functions that return fitted bspline as dict.
-  m.def("interpolate_curve",
-        &splinepy::py::InterpolateCurve,
-        py::arg("points"),
-        py::arg("degree"),
-        py::arg("centripetal"),
-        py::arg("knot_vector"));
-  m.def("approximate_curve",
-        &splinepy::py::ApproximateCurve,
-        py::arg("points"),
-        py::arg("degree"),
-        py::arg("n_control_points"),
-        py::arg("centripetal"),
-        py::arg("knot_vector"));
-  m.def("interpolate_surface",
-        &splinepy::py::InterpolateSurface,
-        py::arg("points"),
-        py::arg("size_u"),
-        py::arg("size_v"),
-        py::arg("degree_u"),
-        py::arg("degree_v"),
-        py::arg("centripetal"));
-  m.def("approximate_surface",
-        &splinepy::py::ApproximateSurface,
-        py::arg("points"),
-        py::arg("num_points_u"),
-        py::arg("num_points_v"),
-        py::arg("size_u"),
-        py::arg("size_v"),
-        py::arg("degree_u"),
-        py::arg("degree_v"),
-        py::arg("centripetal"));
-}
+void init_fitting(py::module_& m) { add_fitting(m); }
 
 } // namespace splinepy::py::init
