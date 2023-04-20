e5d84b3d3d6411d2b813e3a60b7daf616bccfb0a
Jaewook Lee <jaewooklee042@gmail.com>
2023-04-20 21:19:55 +0200

    T for correct order

--- splinepy/helpme/multi_index.py
+++ splinepy/helpme/multi_index.py
@@ -10,8 +10,7 @@ class MultiIndex:
     __getitem__ like queries.
     """
 
-    __slots__ = ("_raveled_indices", "_ndim")
-    _ellipsis_type = type(Ellipsis)
+    __slots__ = ("_raveled_indices",)
 
     def __init__(self, grid_resolutions):
         """
@@ -33,8 +32,7 @@ class MultiIndex:
 
         # to allow general __getitem__ like query, turn them into
         # grid's shape
-        self._raveled_indices = raveled.reshape(*grid_resolutions[::-1])
-        self._ndim = self._raveled_indices.ndim
+        self._raveled_indices = raveled.reshape(*grid_resolutions, order="F")
 
     def __getitem__(self, args):
         """
@@ -50,26 +48,4 @@ class MultiIndex:
         raveled_indices: np.ndarray
           raveled indices, which are in a raveled array.
         """
-        # only interested in __iter__, not __getitem__
-        if not hasattr(args, "__iter__"):
-            args = [args]
-        else:
-            args = list(args)
-
-        # Ellipsis? don't do anything
-        # (Ellipsis not in args) raises ValueError during comparison
-        # if one of them is an array. so, loop and check if any of the entry
-        # are Ellipsis
-        fill_slices = True
-        for a in args:
-            if isinstance(a, self._ellipsis_type):
-                fill_slices = False
-                break
-
-        # no ellipsis? fill in with slices
-        if fill_slices:
-            s = slice(None)
-            for _ in range(len(args), self._ndim):
-                args.append(s)
-
-        return self._raveled_indices.__getitem__(tuple(args[::-1])).ravel()
+        return self._raveled_indices.__getitem__(args).T.ravel()
