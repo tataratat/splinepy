64a63c5761407e9ac952992dc7bbba2778bd543e
Jaewook Lee <jaewooklee042@gmail.com>
2023-05-10 16:51:19 +0200

    rough draft of even-ish load

--- cpp/splinepy/py/py_spline_list.hpp
+++ cpp/splinepy/py/py_spline_list.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <cstdlib>
+
 // pybind
 #include <pybind11/pybind11.h>
 #include <pybind11/stl_bind.h>
 
 #include <splinepy/py/py_spline.hpp>
+#include <splinepy/utils/nthreads.hpp>
 
 // PYBIND11_MAKE_OPAQUE(std::vector<std::shared_ptr<splinepy::py::PySpline>>);
 
@@ -14,16 +17,58 @@ namespace py = pybind11;
 
 using PySplineList = std::vector<std::shared_ptr<PySpline>>;
 
-inline py::array_t<double>
-Evaluate(const PySplineList& splist,
-         const py::array_t<double>& queries,
-         const int nthreads) {}
+// evaluates splines of same para_dim and dim.
+inline py::array_t<double> Evaluate(const PySplineList& splist,
+                                    const py::array_t<double>& queries,
+                                    const int nthreads) {
+  const int& para_dim = splist[0]->para_dim_;
+  const int& dim = splist[0]->dim_;
 
-inline py::array_t<double>
-Derivative(const PySplineList& splist,
-           const py::array_t<double>& queries,
-           const py::array_t<int>& orders,
-           const int nthreads) {}
+  CheckPyArrayShape(queries, {-1, para_dim}, true);
+
+  // prepare input and output
+  double* queries_ptr = static_cast<double*>(queries.request().ptr);
+  const int n_splines = splist.size();
+  const int n_queries = queries.size();
+  const int n_total = n_splines * n_queries;
+  py::array_t<double> evaluated({n_total, dim});
+  double* evaluated_ptr = static_cast<double*>(evaluated.request().ptr);
+
+  // each thread evaluates similar amount of queries from each spline
+  auto evaluate = [&](int begin, int end) {
+    const int n_common = std::div(end - begin, n_splines).quot; // floor
+    const int spline_start = begin % n_splines;
+    const int spline_end = (end - 1) % n_splines;
+    const int query_offset = std::div(begin, n_splines).quot;
+
+    // loop splines
+    for (int i{}; i < n_splines; ++i) {
+      const auto& core = *splist[i]->Core();
+      const int output_offset = i * n_queries * dim;
+      int query_start = query_offset;
+
+      int n_additional{};
+      if (i < spline_start) {
+        n_additional -= 1;
+        query_start += 1;
+      }
+      if (i <= spline_end) {
+        n_additional += 1;
+      }
+
+      for (int j{query_start}; j < (query_start + n_common + n_additional);
+           ++j) {
+        core.Evaluate(&queries_ptr[j * para_dim],
+                      &evaluated_ptr[output_offset + (j * dim)]);
+      }
+    }
+  }
+}
+
+inline py::array_t<double> Derivative(const PySplineList& splist,
+                                      const py::array_t<double>& queries,
+                                      const py::array_t<int>& orders,
+                                      const int nthreads) {}
 
 inline py::array_t<double> Sample(const PySplineList& splist,
                                   const py::array_t<int> resolutions,
