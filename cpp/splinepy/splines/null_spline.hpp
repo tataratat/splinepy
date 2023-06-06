be9605d8efefecd983f82aa7bbdc796e97d346ff
Jaewook Lee <jaewooklee042@gmail.com>
2023-06-06 22:48:51 +0200

    move static lookup outside the class

--- cpp/splinepy/splines/null_spline.hpp
+++ cpp/splinepy/splines/null_spline.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <algorithm>
+#include <array>
+#include <memory>
 
 #include <splinepy/splines/splinepy_base.hpp>
 
@@ -14,29 +16,11 @@ namespace splinepy::splines {
 /// For example, if you have numerous list of
 class NullSpline : public splinepy::splines::SplinepyBase {
 public:
-  using LookupArray_ =
-      std::array<std::array<std::shared_ptr<SplinepyBase>, kMaxLookupDim_>,
-                 kMaxLookupDim_>;
-
   /// parametric and physical dimension needs to be known at creation
   /// with these two, for whatever reason, if they are exposed to python side,
   /// it is still possible to call implemented query functions
   const int para_dim_;
   const int dim_;
-
-  /// pre-create null splines up to the dimension we support
-  constexpr static LookupArray_ lookup_ = [] {
-    LookupArray_ lookup;
-    int i{1};
-    for (auto& para_dim_array : lookup) {
-      int j{1};
-      for (auto& dim_element : para_dim_array) {
-        dim_element = std::make_shared<NullSpline>(i, j);
-        ++j;
-      }
-      ++i;
-    }
-  }();
 
   /// @brief ctor with parametric and physical dimension
   /// @param para_dim
@@ -69,22 +53,46 @@ public:
   virtual int SplinepyNumberOfControlPoints() const { return -1; }
   virtual int SplinepyNumberOfSupports() const { return -1; }
   virtual bool SplinepyIsNull() const { return true; }
+  virtual void
+  SplinepyCurrentProperties(int* degrees,
+                            std::vector<std::vector<double>>* knot_vectors,
+                            double* control_points,
+                            double* weights) const {
+    splinepy::utils::PrintAndThrowError(
+        "SplinepyCurrentProperties() - invalid function call for",
+        SplinepyWhatAmI());
+  }
 
-  /// Spline evaluation
+  /// Spline evaluation - fills zeros.
   virtual void SplinepyEvaluate(const double* para_coord,
                                 double* evaluated) const {
 
     std::fill_n(evaluated, dim_, 0.0);
   }
-
-protected:
-// maxdim to pre-create null splines
-#ifdef SPLINEPY_MORE
-  constexpr static int kMaxLookupDim_{3};
-#else
-  constexpr static int kMaxLookupDim_{10};
-#enif
 };
 
+// maxdim to pre-create null splines
+#ifdef SPLINEPY_MORE
+constexpr static int kMaxLookupDim{3};
+#else
+constexpr static int kMaxLookupDim{10};
+#endif
+using LookupArray_ =
+    std::array<std::array<std::shared_ptr<SplinepyBase>, kMaxLookupDim>,
+               kMaxLookupDim>;
+/// pre-create null splines up to the dimension we support
+static const LookupArray_ kNullSplineLookup = [] {
+  LookupArray_ lookup;
+  int i{0};
+  for (auto& para_dim_array : lookup) {
+    int j{0};
+    for (auto& dim_element : para_dim_array) {
+      dim_element = std::make_shared<NullSpline>(i + 1, j + 1);
+      ++j;
+    }
+    ++i;
+  }
+  return lookup;
+}();
 
-} // splinepy::splines
+} // namespace splinepy::splines
