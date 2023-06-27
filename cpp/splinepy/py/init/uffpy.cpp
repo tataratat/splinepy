12fc6fdd7914902e1c67fa3fc112ce1c788fd2b3
Jaewook Lee <jaewooklee042@gmail.com>
2023-06-27 10:49:51 +0200

    rm uff

--- cpp/splinepy/py/init/uffpy.cpp
+++
@@ -1,8 +0,0 @@
-#include <splinepy/py/py_uffpy.hpp>
-
-namespace splinepy::py::init {
-
-namespace py = pybind11;
-void init_uffpy(py::module_& m) { add_uffpy(m); }
-
-} // namespace splinepy::py::init
