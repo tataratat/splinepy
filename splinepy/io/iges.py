1a4a8d13296ab281ef4cd2d1f4c2b0711b5da055
mkofler96 <rasta.kof@gmail.com>
2023-10-09 11:42:06 +0200

    fixed linewidth in splinepy/io/iges.py

--- splinepy/io/iges.py
+++ splinepy/io/iges.py
@@ -38,7 +38,9 @@ def export(fname, splines):
     for ispl, spline in enumerate(splines):
         if spline.para_dim > 2 or spline.dim > 3:
             raise ValueError(
-                f"Volumetric {type(spline)} found at splines[{ispl}], which cannot be handled by the .iges file format \n"
-                "Use a boundary representation instead by calling e.g. multipatch.boundary_multipatch()"
+                f"Volumetric {type(spline)} found at splines[{ispl}]"
+                 "which cannot be handled by the .iges file format \n"
+                "Use a boundary representation instead by calling e.g."
+                "multipatch.boundary_multipatch()"
             )
     return splinepy_core.export_iges(fname, splines)
