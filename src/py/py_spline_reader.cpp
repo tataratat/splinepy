13b615304af198460322328d586891c2b9c740b7
Jaewook Lee <jaewooklee042@gmail.com>
2023-10-25 15:26:34 +0200

    update iges and bsplinelib commit

--- src/py/py_spline_reader.cpp
+++ src/py/py_spline_reader.cpp
@@ -1,330 +1,81 @@
 #include <memory>
-#include <vector>
 
+#include <BSplineLib/InputOutput/iges.hpp>
+#include <BSplineLib/InputOutput/operations.hpp>
+#include <BSplineLib/Splines/spline_item.hpp>
+
+#include "splinepy/py/py_spline.hpp"
 #include "splinepy/py/py_spline_reader.hpp"
+#include "splinepy/splines/bspline.hpp"
+#include "splinepy/splines/nurbs.hpp"
 #include "splinepy/utils/print.hpp"
 
 namespace splinepy::py {
 
-/// @brief Convert BSpline to Python dictionary
-/// @param bspline BSpline object
-template<int para_dim, int dim>
-py::dict
-BSplineParser<para_dim, dim>::BSplineToDict(SplineEntry const& bspline) {
-  int i, j;
+template<int para_dim>
+std::shared_ptr<PySpline>
+ConvertToPySpline(std::shared_ptr<bsplinelib::splines::SplineItem>& spline) {
+  // rational -> NURBS
+  if (spline->is_rational_) {
+    auto bsplinelib_nurbs =
+        bsplinelib::input_output::operations::CastToSpline<para_dim, true>(
+            spline);
+    auto splinepy_nurbs =
+        std::make_shared<splinepy::splines::Nurbs<para_dim>>();
+    // with proper default init, we can just take members as they are shared
+    splinepy_nurbs->ShareMembers(bsplinelib_nurbs);
 
-  py::dict spline; //  to return
-
-  std::shared_ptr<BSpline> bs = std::dynamic_pointer_cast<BSpline>(bspline);
-  OutputInformation const& bs_info = bs->Write();
-
-  //
-  // Adapted from `bspline.hpp`
-  //
-
-  // Parameter space - knot vectors, degrees
-  OutputParameterSpace const& parameter_space = std::get<0>(bs_info);
-  OutputKnotVectors const& knot_vectors = std::get<0>(parameter_space);
-  OutputDegrees const& degrees = std::get<1>(parameter_space);
-
-  // Vector space - Coordinates(control points)
-  OutputVectorSpace const& vector_space = std::get<1>(bs_info);
-  OutputCoordinates const& coordinates = std::get<0>(vector_space);
-
-  // Unpack - degrees
-  auto p_degrees = py::array_t<int>(para_dim);
-  py::buffer_info ds_buf = p_degrees.request();
-  int* ds_buf_ptr = static_cast<int*>(ds_buf.ptr);
-
-  i = 0;
-  for (auto& degree : degrees) {
-    ds_buf_ptr[i] = utilities::string_operations::ConvertToNumber<int>(degree);
-    i++;
+    return std::make_shared<PySpline>(splinepy_nurbs);
   }
 
-  spline["degrees"] = p_degrees;
+  auto bsplinelib_bspline =
+      bsplinelib::input_output::operations::CastToSpline<para_dim, false>(
+          spline);
+  auto splinepy_bspline =
+      std::make_shared<splinepy::splines::BSpline<para_dim>>();
+  splinepy_bspline->ShareMembers(bsplinelib_bspline);
 
-  // Unpack - knot vectors
-  py::list p_knot_vectors;
-  for (auto& knotvector : knot_vectors) {
-    py::list p_kv;
-    for (auto& knot : knotvector) {
-      p_kv.append(utilities::string_operations::ConvertToNumber<double>(knot));
-    }
-    p_knot_vectors.append(p_kv);
-  }
-
-  spline["knot_vectors"] = p_knot_vectors;
-
-  // Unpack - Coordinates (control points)
-  auto p_control_points = py::array_t<double>(coordinates.size() * dim);
-  py::buffer_info cps_buf = p_control_points.request();
-  double* cps_buf_ptr = static_cast<double*>(cps_buf.ptr);
-
-  i = 0;
-  for (auto& coordinate : coordinates) {
-    j = 0;
-    for (auto& coord : coordinate) {
-      cps_buf_ptr[i * dim + j] =
-          utilities::string_operations::ConvertToNumber<double>(coord);
-      j++;
-    }
-    i++;
-  }
-
-  p_control_points.resize({(int) coordinates.size(), dim});
-
-  spline["control_points"] = p_control_points;
-
-  return spline;
+  // else -> BSpline
+  return std::make_shared<PySpline>(splinepy_bspline);
 }
 
-/// @brief BSpline parser class
-/// @tparam para_dim Dimension of parametric space
-/// @tparam dim Dimension of physical space
-template<int para_dim, int dim>
-py::dict NurbsParser<para_dim, dim>::NurbsToDict(SplineEntry const& nurbs) {
-  int i, j;
-
-  py::dict spline; //  to return
-
-  // Read info
-  std::shared_ptr<Nurbs> n = std::dynamic_pointer_cast<Nurbs>(nurbs);
-  OutputInformation const& n_info = n->Write();
-
-  //
-  // Adapted from `nurbs.hpp`
-  //
-
-  // Parameter space - knot vectors, degrees
-  OutputParameterSpace const& parameter_space = std::get<0>(n_info);
-  OutputKnotVectors const& knot_vectors = std::get<0>(parameter_space);
-  OutputDegrees const& degrees = std::get<1>(parameter_space);
-
-  // Weighted vector space - Coordinates(control points), weights
-  OutputWeightedVectorSpace const& weighted_vector_space = std::get<1>(n_info);
-  OutputCoordinates const& coordinates = std::get<0>(weighted_vector_space);
-  OutputWeights const& weights = std::get<1>(weighted_vector_space);
-
-  // Unpack - Weights
-  auto p_weights = py::array_t<double>(weights.size());
-  py::buffer_info ws_buf = p_weights.request();
-  double* ws_buf_ptr = static_cast<double*>(ws_buf.ptr);
-
-  i = 0;
-  for (auto& weight : weights) {
-    ws_buf_ptr[i] =
-        utilities::string_operations::ConvertToNumber<double>(weight);
-    i++;
-  }
-
-  p_weights.resize({(int) weights.size(), 1}); // A tall vector
-
-  spline["weights"] = p_weights;
-
-  // Unpack - degrees
-  auto p_degrees = py::array_t<int>(para_dim);
-  py::buffer_info ds_buf = p_degrees.request();
-  int* ds_buf_ptr = static_cast<int*>(ds_buf.ptr);
-
-  i = 0;
-  for (auto& degree : degrees) {
-    ds_buf_ptr[i] = utilities::string_operations::ConvertToNumber<int>(degree);
-    i++;
-  }
-
-  spline["degrees"] = p_degrees;
-
-  // Unpack - knot vectors
-  py::list p_knot_vectors;
-  for (auto& knotvector : knot_vectors) {
-    py::list p_kv;
-    for (auto& knot : knotvector) {
-      p_kv.append(utilities::string_operations::ConvertToNumber<double>(knot));
-    }
-    p_knot_vectors.append(p_kv);
-  }
-
-  spline["knot_vectors"] = p_knot_vectors;
-
-  // Unpack - Coordinates (control points)
-  auto p_control_points = py::array_t<double>(coordinates.size() * dim);
-  py::buffer_info cps_buf = p_control_points.request();
-  double* cps_buf_ptr = static_cast<double*>(cps_buf.ptr);
-
-  i = 0;
-  for (auto& coordinate : coordinates) {
-    j = 0;
-    for (auto& coord : coordinate) {
-      cps_buf_ptr[i * dim + j] =
-          utilities::string_operations::ConvertToNumber<double>(coord);
-      j++;
-    }
-    i++;
-  }
-
-  p_control_points.resize({(int) coordinates.size(), dim});
-
-  spline["control_points"] = p_control_points;
-
-  return spline;
-}
-
-py::list SplineReader::ReadIges(std::string fname) {
-  int i, j;
-
-  auto c_splines = input_output::iges::Read(fname);
-  py::list dict_splines;
-
-  for (auto& spline : c_splines) {
-    if (spline->is_rational_) {
-      dict_splines.append(ParseNurbs(spline));
-    } else {
-      dict_splines.append(ParseBSpline(spline));
-    }
-  }
-  return dict_splines;
-}
-
-py::dict SplineReader::ParseBSpline(SplineEntry const& bspline) {
-
-  // Get paradim and dim
-  int const& para_dim = bspline->parametric_dimensionality_;
-  int const& dim = bspline->dimensionality_;
-
-  // Find appropriate BSplines
-  switch (para_dim) {
+std::shared_ptr<PySpline>
+ToPySpline(std::shared_ptr<bsplinelib::splines::SplineItem>& spline) {
+  switch (spline->parametric_dimensionality_) {
   case 1:
-    switch (dim) {
-    case 1:
-      return BSplineParser<1, 1>::BSplineToDict(bspline);
-    case 2:
-      return BSplineParser<1, 2>::BSplineToDict(bspline);
-    case 3:
-      return BSplineParser<1, 3>::BSplineToDict(bspline);
-    default:
-      splinepy::utils::PrintAndThrowError(
-          "ParseBSpline() does not support physical dimension of ",
-          dim,
-          ". Only dimensions 1 to 3 are supported.");
-      break;
-    }
-    break;
+    return ConvertToPySpline<1>(spline);
   case 2:
-    switch (dim) {
-    case 1:
-      return BSplineParser<2, 1>::BSplineToDict(bspline);
-    case 2:
-      return BSplineParser<2, 2>::BSplineToDict(bspline);
-    case 3:
-      return BSplineParser<2, 3>::BSplineToDict(bspline);
-    default:
-      splinepy::utils::PrintAndThrowError(
-          "ParseBSpline() does not support physical dimension of ",
-          dim,
-          ". Only dimensions 1 to 3 are supported.");
-      break;
-    }
-    break;
-  case 3:
-    switch (dim) {
-    case 1:
-      return BSplineParser<3, 1>::BSplineToDict(bspline);
-    case 2:
-      return BSplineParser<3, 2>::BSplineToDict(bspline);
-    case 3:
-      return BSplineParser<3, 3>::BSplineToDict(bspline);
-    default:
-      splinepy::utils::PrintAndThrowError(
-          "ParseBSpline() does not support physical dimension of ",
-          dim,
-          ". Only dimensions 1 to 4 are supported.");
-      break;
-    }
-    break;
+    return ConvertToPySpline<2>(spline);
   default:
     splinepy::utils::PrintAndThrowError(
-        "ParseBSpline() does not support parametric dimension of ",
-        para_dim,
-        ". Only dimensions 1 to 3 are supported.");
+        "invalid para_dim (",
+        spline->parametric_dimensionality_,
+        ") detected while loading spline. It should be less then 3.");
     break;
   }
-
-  return py::dict{};
+  return std::shared_ptr<PySpline>{};
 }
 
-py::dict SplineReader::ParseNurbs(SplineEntry const& nurbs) {
+py::list ReadIges(const std::string fname) {
+  // read
+  py::list splines;
 
-  // Get paradim and dim
-  int const& para_dim = nurbs->parametric_dimensionality_;
-  int const& dim = nurbs->dimensionality_;
+  // load
+  auto c_splines = bsplinelib::input_output::iges::Read(fname);
 
-  // Find appropriate Nurbs
-  switch (para_dim) {
-  case 1:
-    switch (dim) {
-    case 1:
-      return NurbsParser<1, 1>::NurbsToDict(nurbs);
-    case 2:
-      return NurbsParser<1, 2>::NurbsToDict(nurbs);
-    case 3:
-      return NurbsParser<1, 3>::NurbsToDict(nurbs);
-    default:
-      splinepy::utils::PrintAndThrowError(
-          "ParseNurbs() does not support physical dimension of ",
-          dim,
-          ". Only dimensions 1 to 3 are supported.");
-      break;
-    }
-    break;
-  case 2:
-    switch (dim) {
-    case 1:
-      return NurbsParser<2, 1>::NurbsToDict(nurbs);
-    case 2:
-      return NurbsParser<2, 2>::NurbsToDict(nurbs);
-    case 3:
-      return NurbsParser<2, 3>::NurbsToDict(nurbs);
-    default:
-      splinepy::utils::PrintAndThrowError(
-          "ParseNurbs() does not support physical dimension of ",
-          dim,
-          ". Only dimensions 1 to 3 are supported.");
-      break;
-    }
-    break;
-  case 3:
-    switch (dim) {
-    case 1:
-      return NurbsParser<3, 1>::NurbsToDict(nurbs);
-    case 2:
-      return NurbsParser<3, 2>::NurbsToDict(nurbs);
-    case 3:
-      return NurbsParser<3, 3>::NurbsToDict(nurbs);
-    default:
-      splinepy::utils::PrintAndThrowError(
-          "ParseNurbs() does not support physical dimension of ",
-          dim,
-          ". Only dimensions 1 to 4 are supported.");
-      break;
-    }
-    break;
-  default:
-    splinepy::utils::PrintAndThrowError(
-        "ParseNurbs() does not support parametric dimension of ",
-        para_dim,
-        ". Only dimensions 1 to 3 are supported.");
-    break;
+  // convert
+  for (std::shared_ptr<bsplinelib::splines::SplineItem>& spline : c_splines) {
+    splines.append(ToPySpline(spline)->ToDerived());
   }
 
-  return py::dict{};
+  return splines;
 }
 
 ///  @brief Adds spline reader. Keys are
 /// ["knot_vectors", "control_points", "degrees"] (+ ["weights"])
 /// @param m Python module
 void init_spline_reader(py::module_& m) {
-  m.def("read_iges", &SplineReader::ReadIges, py::arg("fname"));
+  m.def("read_iges", &ReadIges, py::arg("fname"));
 }
 
 } // namespace splinepy::py
