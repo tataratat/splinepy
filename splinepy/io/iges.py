d2179057fae58f1d1e0ab54f3f12149e04fe922b
Jaewook Lee <jaewooklee042@gmail.com>
2023-11-07 15:00:29 +0100

    changed to warnings and only save valid splines

--- splinepy/io/iges.py
+++ splinepy/io/iges.py
@@ -2,6 +2,7 @@
 iges io.
 """
 from splinepy import splinepy_core
+from splinepy.utils import log
 
 
 def load(fname):
@@ -34,13 +35,27 @@ def export(fname, splines):
     --------
     None
     """
+    # create empty valid spline array
+    valid_splines = []
     # check if any spline contains volume elements
     for ispl, spline in enumerate(splines):
         if spline.para_dim > 2 or spline.dim > 3:
-            raise ValueError(
-                f"Volumetric {type(spline)} found at splines[{ispl}]"
+            log.warning(
+                f"Skipping volumetric {type(spline)} found at splines[{ispl}]"
                 "which cannot be handled by the .iges file format \n"
-                "Use a boundary representation instead by calling e.g."
-                "spline.extract.boundaries()"
+                "To include this spline, use a boundary representation "
+                "instead, by calling e.g. spline.extract.boundaries()"
             )
-    return splinepy_core.export_iges(fname, splines)
+        elif spline.para_dim > spline.dim:
+            log.warning(
+                f"Skipping {type(spline)} at splines[{ispl}]\n"
+                f"Parametric dimension ({spline.para_dim}) is higher than the"
+                f" physical dimension ({spline.dim}) at splines[{ispl}]\n"
+                " Parametric dimension must be smaller or equal to the"
+                " physical dimension."
+            )
+        else:
+            valid_splines.append(spline)
+    if not valid_splines:
+        log.warning("No valid splines found in the given input")
+    return splinepy_core.export_iges(fname, valid_splines)
