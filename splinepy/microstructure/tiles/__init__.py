b95aea4d937fce0dcefb174d6a6c2a46c0b7f0b4
jzwar <zwar@ilsb.tuwien.ac.at>
2023-11-14 12:09:32 +0100

    Renamed all files according to style guide
    Renaming all tiles and adapted their file names to fit the style guide

--- splinepy/microstructure/tiles/__init__.py
+++ splinepy/microstructure/tiles/__init__.py
@@ -5,54 +5,56 @@ Interface for tools and generators creating simple microstructures.
 
 from splinepy.microstructure.tiles import (
     armadillo,
-    crosstile2d,
-    crosstile3d,
-    cube3d,
-    cubevoid,
-    double_lattice_tile,
-    ellipsvoid,
-    inversecrosstile3d,
-    nuttile2d,
-    nuttile3d,
-    snappytile,
-    tilebase,
+    cross_2d,
+    cross_3d,
+    cube_void,
+    double_lattice,
+    ellipse_void,
+    hollow_cube,
+    hollow_octagon,
+    hollow_octagon_extrude,
+    inverse_cross_3d,
+    snappy,
+    tile_base,
 )
 from splinepy.microstructure.tiles.armadillo import Armadillo
-from splinepy.microstructure.tiles.crosstile2d import CrossTile2D
-from splinepy.microstructure.tiles.crosstile3d import CrossTile3D
-from splinepy.microstructure.tiles.cube3d import Cube3D
-from splinepy.microstructure.tiles.cubevoid import Cubevoid
-from splinepy.microstructure.tiles.double_lattice_tile import DoubleLatticeTile
-from splinepy.microstructure.tiles.ellipsvoid import Ellipsvoid
-from splinepy.microstructure.tiles.inversecrosstile3d import InverseCrossTile3D
-from splinepy.microstructure.tiles.nuttile2d import NutTile2D
-from splinepy.microstructure.tiles.nuttile3d import NutTile3D
-from splinepy.microstructure.tiles.snappytile import SnappyTile
-from splinepy.microstructure.tiles.tilebase import TileBase
+from splinepy.microstructure.tiles.cross_2d import Cross2D
+from splinepy.microstructure.tiles.cross_3d import Cross3D
+from splinepy.microstructure.tiles.cube_void import CubeVoid
+from splinepy.microstructure.tiles.double_lattice import DoubleLattice
+from splinepy.microstructure.tiles.ellipse_void import EllipseVoid
+from splinepy.microstructure.tiles.hollow_cube import HollowCube
+from splinepy.microstructure.tiles.hollow_octagon import HollowOctagon
+from splinepy.microstructure.tiles.hollow_octagon_extrude import (
+    HollowOctagonExtrude,
+)
+from splinepy.microstructure.tiles.inverse_cross_3d import InverseCross3D
+from splinepy.microstructure.tiles.snappy import Snappy
+from splinepy.microstructure.tiles.tile_base import TileBase
 
 __all__ = [
     "armadillo",
-    "crosstile2d",
-    "crosstile3d",
-    "cube3d",
-    "cubevoid",
-    "double_lattice_tile",
-    "ellipsvoid",
-    "inversecrosstile3d",
-    "nuttile2d",
-    "nuttile3d",
-    "snappytile",
-    "tilebase",
+    "cross_2d",
+    "cross_3d",
+    "cube_void",
+    "double_lattice",
+    "ellipse_void",
+    "hollow_cube",
+    "hollow_octagon",
+    "hollow_octagon_extrude",
+    "inverse_cross_3d",
+    "snappy",
+    "tile_base",
     "Armadillo",
-    "CrossTile2D",
-    "CrossTile3D",
-    "Cube3D",
-    "Cubevoid",
-    "DoubleLatticeTile",
-    "Ellipsvoid",
-    "InverseCrossTile3D",
-    "NutTile2D",
-    "NutTile3D",
-    "SnappyTile",
+    "Cross2D",
+    "Cross3D",
+    "CubeVoid",
+    "DoubleLattice",
+    "EllipseVoid",
+    "HollowCube",
+    "HollowOctagon",
+    "HollowOctagonExtrude",
+    "InverseCross3D",
+    "Snappy",
     "TileBase",
 ]
--- splinepy/microstructure/tiles/armadillo.py
+++ splinepy/microstructure/tiles/armadillo.py
@@ -1,7 +1,8 @@
 import numpy as _np
 
+
 from splinepy.bezier import Bezier as _Bezier
-from splinepy.microstructure.tiles.tilebase import TileBase as _TileBase
+from splinepy.microstructure.tiles.tile_base import TileBase as _TileBase
 
 
 class Armadillo(_TileBase):
