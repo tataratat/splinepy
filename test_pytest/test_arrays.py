2f7145a170c5fed9963ad9ada1dd526f00795805
clemens.fricke <clemens.david.fricke@tuwien.ac.at>
2024-02-08 18:22:53 +0100

    Fix: Generator initialization, Add: warning supression for jupyter_client, Add: config to run just test_pytest folder

--- test_pytest/test_arrays.py
+++ test_pytest/test_arrays.py
@@ -9,9 +9,10 @@ def test_uniform_query():
     Test uniform query against gustaf's vertice creation
     """
     for i in range(1, 11):
-        l_bound = np.random.random(i)
+        rng = np.random.default_rng()
+        l_bound = rng.random(i)
         bounds = [l_bound, l_bound + 1]
-        res = np.random.Generator([3] * i) + 2  # minimum 2
+        res = rng.integers([3] * i) + 2  # minimum 2
         gus_q = gus.create.vertices.raster(bounds, res).vertices
         spp_q = splinepy.utils.data.uniform_query(bounds, res)
 
