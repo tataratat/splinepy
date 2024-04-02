b77d9a22444626dee5807d837275c3de9379eca7
Guenther Obermair <guenther.obermair@tuwien.ac.at>
2024-04-02 15:08:45 +0200

    Add: test_multipatch-file and changed it to pytest; Note: 3 errors due to super() has no attribute interfaces

---
+++ test_pytest/test_multipatch.py
@@ -0,0 +1,204 @@
+import numpy as np
+
+import splinepy
+
+
+class SplineSetup:
+    def __init__(self):
+        # Spline that points to itself
+        self._self_referencing_spline = splinepy.Bezier(
+            degrees=[3, 1],
+            control_points=[
+                [0, 0],
+                [2, 3],
+                [-2, 3],
+                [0, 0],
+                [0, 1],
+                [1, 2],
+                [-1, 2],
+                [0, 1],
+            ],
+        )
+
+        # List of splinewith with this topology
+        #
+        #  o ---- o ---- o
+        #  |   0  |   1  |
+        #  o ---- o ---- o
+        #  |   2  |
+        #  o ---- o
+        #
+        self._rect_arc_1 = splinepy.Bezier(
+            [2, 1], [[0, 0], [1, 0], [2, 0], [0, 1], [1, 2], [2, 1]]
+        )
+        self._rect_arc_2 = splinepy.Bezier(
+            [1, 1], [[2, 0], [3, 0], [2, 1], [3, 1]]
+        )
+        self._rect_arc_3 = splinepy.Bezier(
+            [2, 1], [[0, -1], [1, -2], [2, -1], [0, 0], [1, 0], [2, 0]]
+        )
+        self._list_of_splines = [
+            self._rect_arc_1,
+            self._rect_arc_2,
+            self._rect_arc_3,
+        ]
+
+
+spline = SplineSetup()
+
+
+def test_interfaces():
+    """ """
+    # init multipatch with multiple splines
+    multipatch = splinepy.Multipatch()
+    multipatch.patches = spline._list_of_splines
+
+    # init multipatch with single spline
+    single_p_multipatch = splinepy.Multipatch()
+    single_p_multipatch.patches = [spline._self_referencing_spline]
+
+    # Determine connectivities
+    single_p_multipatch.determine_interfaces()
+    multipatch.determine_interfaces()
+    assert (
+        single_p_multipatch.interfaces
+        == np.array([[1, 0, -1, -1]], dtype=int).all()
+    )
+
+    assert (
+        multipatch.interfaces
+        == np.array(
+            # Global Face IDs and their connectivity
+            # [0  1   2   3]  [4   5   6   7]   [8   9  10  11]
+            [[-1, 4, 11, -1], [1, -1, -1, -1], [-1, -1, -1, 2]],
+            dtype=int,
+        ).all()
+    )
+
+
+def test_boundaries():
+    """ """
+    # init multipatch with multiple splines
+    multipatch = splinepy.Multipatch()
+    multipatch.patches = spline._list_of_splines
+    multipatch.determine_interfaces()
+
+    # Using a function
+    def west_side(points):
+        return points[:, 0] < 0.1
+
+    multipatch.boundary_from_function(west_side)
+    assert (
+        multipatch.interfaces
+        == np.array(
+            # Global Face IDs and their connectivity
+            # [0  1   2   3]  [4   5   6   7]   [8   9  10  11]
+            [[-2, 4, 11, -1], [1, -1, -1, -1], [-2, -1, -1, 2]],
+            dtype=int,
+        ).all()
+    )
+
+    multipatch.set_boundary([0, 1], [3, 3], 3)
+    multipatch.set_boundary([1], [1])
+    assert (
+        multipatch.interfaces
+        == np.array(
+            # Global Face IDs and their connectivity
+            # [0  1   2   3]  [4   5   6   7]   [8   9  10  11]
+            [[-2, 4, 11, -3], [1, -4, -1, -3], [-2, -1, -1, 2]],
+            dtype=int,
+        ).all()
+    )
+
+    # Delete all boundaries and determine new ones based on continuity
+    multipatch.boundaries_from_continuity()
+    assert (
+        multipatch.interfaces
+        == np.array(
+            # Global Face IDs and their connectivity
+            # [0  1   2   3]  [4   5   6   7]   [8   9  10  11]
+            [[-1, 4, 11, -2], [1, -3, -4, -5], [-1, -6, -7, 2]],
+            dtype=int,
+        ).all()
+    )
+
+
+def test_interfaces_and_boundaries():
+    # 2 --- 3 1 --- 0
+    # |  1  | |  3  |
+    # 0 --- 1 3 --- 2
+    # 3 --- 2 0 --- 2
+    # |  2  | |  4  |
+    # 1 --- 0 1 --- 3
+    #
+    # with 1 and three flipped in the 3rd axis
+
+    # Create some splines in a random parametric swaps
+    b1 = splinepy.Bezier(
+        degrees=[1, 1, 1],
+        control_points=[
+            [-2, 0, 3],
+            [0, 0, 3],
+            [-2, 1, 3],
+            [0, 1, 3],
+            [-2, 0, 0],
+            [0, 0, 0],
+            [-2, 1, 0],
+            [0, 1, 0],
+        ],
+    )
+    b2 = splinepy.Bezier(
+        degrees=[1, 1, 1],
+        control_points=[
+            [0, -1, 0],
+            [-2, -1, 0],
+            [0, 0, 0],
+            [-2, 0, 0],
+            [0, -1, 3],
+            [-2, -1, 3],
+            [0, 0, 3],
+            [-2, 0, 3],
+        ],
+    )
+    b3 = splinepy.Bezier(
+        degrees=[1, 1, 1],
+        control_points=[
+            [1, 1, 3],
+            [0, 1, 3],
+            [1, 0, 3],
+            [0, 0, 3],
+            [1, 1, 0],
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, 0, 0],
+        ],
+    )
+    b4 = splinepy.Bezier(
+        degrees=[1, 1, 1],
+        control_points=[
+            [0, 0, 0],
+            [0, -1, 0],
+            [1, 0, 0],
+            [1, -1, 0],
+            [0, 0, 3],
+            [0, -1, 3],
+            [1, 0, 3],
+            [1, -1, 3],
+        ],
+    )
+
+    # Multipatch
+    multipatch = splinepy.Multipatch([b1, b2, b3, b4])
+    multipatch.boundaries_from_continuity()
+    assert (
+        multipatch.interfaces
+        == np.array(
+            [
+                [-1, 13, 9, -2, -3, -4],
+                [20, -1, -5, 2, -4, -3],
+                [-6, 1, -2, 18, -3, -4],
+                [15, -5, 6, -6, -4, -3],
+            ],
+            dtype=int,
+        ).all()
+    )
