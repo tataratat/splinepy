dfaff7706aff556eeedf04b7903c50b659977bc8
Jaewook Lee <jaewooklee042@gmail.com>
2024-04-10 12:27:15 +0200

    fix test_kv_manipulation
    copy wasn't a copy

--- test_pytest/test_kv_manipulation.py
+++ test_pytest/test_kv_manipulation.py
@@ -5,8 +5,8 @@ def test_insert_knot(np_rng, are_items_close, bspline_2p2d, nurbs_2p2d):
     """Test the knot insertion function (.insert_knot())."""
 
     # creating a reference copy of the splines
-    bspline_copy = bspline_2p2d
-    nurbs_copy = nurbs_2p2d
+    bspline_original = bspline_2p2d.copy()
+    nurbs_original = nurbs_2p2d.copy()
 
     # reference solutions
     bspline_ref_kv = [
@@ -20,31 +20,10 @@ def test_insert_knot(np_rng, are_items_close, bspline_2p2d, nurbs_2p2d):
 
     # insert knots
     bspline_2p2d.insert_knots(0, [0.2, 0.7])
-    bspline_2p2d.insert_knots(
-        1,
-        [
-            0.2,
-            0.5,
-            0.7,
-        ],
-    )
+    bspline_2p2d.insert_knots(1, [0.2, 0.5, 0.7])
 
-    nurbs_2p2d.insert_knots(
-        0,
-        [
-            0.2,
-            0.5,
-            0.7,
-        ],
-    )
-    nurbs_2p2d.insert_knots(
-        1,
-        [
-            0.2,
-            0.5,
-            0.7,
-        ],
-    )
+    nurbs_2p2d.insert_knots(0, [0.2, 0.5, 0.7])
+    nurbs_2p2d.insert_knots(1, [0.2, 0.5, 0.7])
 
     # test knot_vectors
     assert are_items_close(bspline_2p2d.knot_vectors, bspline_ref_kv)
@@ -55,17 +34,19 @@ def test_insert_knot(np_rng, are_items_close, bspline_2p2d, nurbs_2p2d):
     q2D = np_rng.random((10, 2))
 
     # test evaluation
-    assert np.allclose(bspline_2p2d.evaluate(q2D), bspline_copy.evaluate(q2D))
+    assert np.allclose(
+        bspline_2p2d.evaluate(q2D), bspline_original.evaluate(q2D)
+    )
 
-    assert np.allclose(nurbs_2p2d.evaluate(q2D), nurbs_copy.evaluate(q2D))
+    assert np.allclose(nurbs_2p2d.evaluate(q2D), nurbs_original.evaluate(q2D))
 
 
 def test_insert_knot_with_matrix(np_rng, bspline_2p2d, nurbs_2p2d):
     """Test the knot insertion function (.insert_knot())."""
 
     # creating a reference copy of the splines
-    bspline_copy = bspline_2p2d
-    nurbs_copy = nurbs_2p2d
+    bspline_original = bspline_2p2d.copy()
+    nurbs_original = nurbs_2p2d.copy()
 
     # BSpline Data
     b_spline_knots_0 = np_rng.random(8)
@@ -95,22 +76,26 @@ def test_insert_knot_with_matrix(np_rng, bspline_2p2d, nurbs_2p2d):
     q2D = np_rng.random((50, 2))
 
     # test evaluation
-    assert np.allclose(bspline_2p2d.evaluate(q2D), bspline_copy.evaluate(q2D))
+    assert np.allclose(
+        bspline_2p2d.evaluate(q2D), bspline_original.evaluate(q2D)
+    )
 
-    assert np.allclose(nurbs_2p2d.evaluate(q2D), nurbs_copy.evaluate(q2D))
+    assert np.allclose(nurbs_2p2d.evaluate(q2D), nurbs_original.evaluate(q2D))
 
     # Test control points and weights
     assert np.allclose(
         bspline_2p2d.control_points,
-        matrix_bspline @ bspline_copy.control_points,
+        matrix_bspline @ bspline_original.control_points,
     )
 
-    assert np.allclose(nurbs_2p2d.weights, matrix_nurbs @ nurbs_copy.weights)
+    assert np.allclose(
+        nurbs_2p2d.weights, matrix_nurbs @ nurbs_original.weights
+    )
 
     assert np.allclose(
         nurbs_2p2d.control_points,
         matrix_nurbs
-        @ (nurbs_copy.weights * nurbs_copy.control_points)
+        @ (nurbs_original.weights * nurbs_original.control_points)
         / nurbs_2p2d.weights,
     )
 
@@ -120,79 +105,35 @@ def test_remove_knot(np_rng, are_items_close, bspline_2p2d, nurbs_2p2d):
     This test also depends on the function .insert_knots!"""
 
     # creating a reference copy of the splines
-    bspline_copy = bspline_2p2d
-    nurbs_copy = nurbs_2p2d
+    bspline_original = bspline_2p2d.copy()
+    nurbs_original = nurbs_2p2d.copy()
 
     # insert and remove knots
-    bspline_2p2d.insert_knots(
-        0,
-        [
-            0.2,
-            0.7,
-        ],
-    )
-    bspline_2p2d.insert_knots(
-        1,
-        [
-            0.2,
-            0.5,
-            0.7,
-        ],
-    )
+    bspline_2p2d.insert_knots(0, [0.2, 0.7])
+    bspline_2p2d.insert_knots(1, [0.2, 0.5, 0.7])
     bspline_2p2d.remove_knots(0, [0.2, 0.7])
-    bspline_2p2d.remove_knots(
-        1,
-        [
-            0.2,
-            0.5,
-            0.7,
-        ],
-    )
+    bspline_2p2d.remove_knots(1, [0.2, 0.5, 0.7])
 
-    nurbs_2p2d.insert_knots(
-        0,
-        [
-            0.2,
-            0.5,
-            0.7,
-        ],
-    )
-    nurbs_2p2d.insert_knots(
-        1,
-        [
-            0.2,
-            0.5,
-            0.7,
-        ],
-    )
-    nurbs_2p2d.remove_knots(
-        0,
-        [
-            0.2,
-            0.5,
-            0.7,
-        ],
-    )
-    nurbs_2p2d.remove_knots(
-        1,
-        [
-            0.2,
-            0.5,
-            0.7,
-        ],
-    )
+    nurbs_2p2d.insert_knots(0, [0.2, 0.5, 0.7])
+    nurbs_2p2d.insert_knots(1, [0.2, 0.5, 0.7])
+    nurbs_2p2d.remove_knots(0, [0.2, 0.5, 0.7])
+    nurbs_2p2d.remove_knots(1, [0.2, 0.5, 0.7])
 
     # test knot_vectors
     assert are_items_close(
-        bspline_2p2d.knot_vectors, bspline_copy.knot_vectors
+        bspline_2p2d.knot_vectors, bspline_original.knot_vectors
     )
 
-    assert are_items_close(nurbs_2p2d.knot_vectors, nurbs_copy.knot_vectors)
+    assert are_items_close(
+        nurbs_2p2d.knot_vectors, nurbs_original.knot_vectors
+    )
 
     # use random query points
     q2D = np_rng.random((10, 2))
 
     # test evaluation
-    assert np.allclose(bspline_2p2d.evaluate(q2D), bspline_copy.evaluate(q2D))
+    assert np.allclose(
+        bspline_2p2d.evaluate(q2D), bspline_original.evaluate(q2D)
+    )
 
-    assert np.allclose(nurbs_2p2d.evaluate(q2D), nurbs_copy.evaluate(q2D))
+    assert np.allclose(nurbs_2p2d.evaluate(q2D), nurbs_original.evaluate(q2D))
