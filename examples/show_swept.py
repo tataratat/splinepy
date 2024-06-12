eb67c22e2e6dc84692d072638b231ac2eea0e038
Guenther Obermair <guenther.obermair@tuwien.ac.at>
2024-06-12 17:07:23 +0200

    Big update; Add: nurbs, traj refinement; Del: fitting routine

--- examples/show_swept.py
+++ examples/show_swept.py
@@ -4,97 +4,69 @@ import numpy as np
 import splinepy
 
 if __name__ == "__main__":
-    # trajectory
-    # define degrees
-    ds1 = [2]
-    # define knot vectors
-    kvs1 = [[0.0, 0.0, 0.0, 0.25, 0.75, 1.0, 1.0, 1.0]]
-    # define control points
-    cps1 = np.array(
-        [
-            [0.0, 0.0, 0.0],
-            [5.0, 2.0, 4.0],
-            [10.0, 5.0, 8.0],
-            [15.0, 2.0, 4.0],
-            [20.0, 5.0, 0.0],
-        ]
-    )
+
+    ### TRAJECTORY ###
+    dict_trajectory = {
+        "degrees": [2],
+        "knot_vectors": [[0.0, 0.0, 0.0, 0.333, 0.666, 1.0, 1.0, 1.0]],
+        "control_points": np.array(
+            [
+                [0.0, 0.0, 0.0],
+                [5.0, 5.0, 0.0],
+                [10.0, 7.5, 0.0],
+                [10.0, 5.0, 0.0],
+                [10.0, 0.0, 0.0],
+            ]
+        ),
+    }
 
     # init trajectory as bspline
-    trajectory = splinepy.BSpline(
-        degrees=ds1,
-        knot_vectors=kvs1,
-        control_points=cps1,
-    )
+    trajectory = splinepy.BSpline(**dict_trajectory)
 
-    # define sections along trajectory
-    nsect = len(kvs1[0]) - ds1[0] - 1
+    # insert knots and control points
+    # trajectory.uniform_refine([0], 1)
 
-    # cross section
-    # define degrees
-    ds_cs = [3]
-
-    # define knot vectors
-    kvs_cs = [[0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0]]
-
-    # define control points
-    cps_cs = np.array(
-        [
-            [0.0, 0.0, 0.0],
-            [1.0, 2.0, 0.0],
-            [2.0, 0.0, 0.0],
-            [3.0, -2.0, 0.0],
-            [4.0, 0.0, 0.0],
-        ]
-    )
+    ### CROSS SECTION ###
+    dict_cross_section = {
+        "degrees": [3],
+        "knot_vectors": [[0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0]],
+        "control_points": np.array(
+            [
+                [0.0, 0.0, 0.0],
+                [1.0, 2.0, 0.0],
+                [2.0, 0.0, 0.0],
+                [3.0, -2.0, 0.0],
+                [4.0, 0.0, 0.0],
+            ]
+        ),
+    }
 
     # init cross section as bspline
-    cross_section = splinepy.BSpline(
-        degrees=ds_cs,
-        knot_vectors=kvs_cs,
-        control_points=cps_cs,
-    )
+    cross_section = splinepy.BSpline(**dict_cross_section)
 
-    # user should define the normal vector of the cross section
+    # alternatively, use helpme to create a cross section
+    # cross_section = splinepy.helpme.create.surface_circle(.5).nurbs
+
+    # user can  define the normal vector of the cross section, in case
+    # the cross section is not planar in the x-y plane (default)
     cs_nv = np.array([0, 0, 1])
 
-    # create interpolated surface
-    interpolated_surface, fitting_surface = splinepy.helpme.create.swept(
+    ### SWEEP ###
+    swept_surface = splinepy.helpme.create.swept(
         trajectory=trajectory,
         cross_section=cross_section,
-        nsections=nsect,
+        nsections=20,
         cross_section_normal=cs_nv,
     )
 
-    # testing derivative and evaluation
-    # derivative = trajectory.derivative([[0.5], [1]], [1])
-    # evals = trajectory.evaluate([[0.5]])
-
-    # show
-    def der(data, on):
-        return data.derivative(on, [1])
-
-    trajectory.spline_data["der"] = splinepy.SplineDataAdaptor(
-        trajectory, function=der
-    )
-    trajectory.show_options["arrow_data"] = "der"
-    trajectory.show_options["arrow_data_on"] = np.linspace(0, 1, 20).reshape(
-        -1, 1
-    )
+    ### VISUALIZATION ###
     trajectory.show_options["control_mesh"] = False
     cross_section.show_options["control_mesh"] = False
-    interpolated_surface.show_options["control_mesh"] = False
-    fitting_surface.show_options["control_mesh"] = False
+    swept_surface.show_options["control_mesh"] = False
 
     gus.show(
         ["Trajectory", trajectory],
-        ["Cross section", cross_section],
-        resolution=50,
-    )
-
-    gus.show(
-        ["Trajectory", trajectory],
-        ["Fitting Surface", fitting_surface],
-        ["Fitted Surface", interpolated_surface],
-        resolution=50,
+        ["Cross Section", cross_section],
+        ["Swept Surface", swept_surface],
+        resolution=51,
     )
