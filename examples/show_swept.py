e4dca0b4a7e524cf1a01286d343c2aa1cdd09726
Guenther Obermair <guenther.obermair@tuwien.ac.at>
2024-07-03 11:38:22 +0200

    Add: swept description; Fix: using settings.TOLERANCE

--- examples/show_swept.py
+++ examples/show_swept.py
@@ -10,19 +10,19 @@ if __name__ == "__main__":
     ### TRAJECTORY ###
 
     # arbitrary trajectory
-    # dict_trajectory = {
-    #     "degrees": [2],
-    #     "knot_vectors": [[0.0, 0.0, 0.0, 0.333, 0.666, 1.0, 1.0, 1.0]],
-    #     "control_points": np.array(
-    #         [
-    #             [0.0, 0.0, 0.0],
-    #             [0.0, 0.0, 5.0],
-    #             [10.0, 5.0, 0.0],
-    #             [15.0, 0.0, -5.0],
-    #             [20.0, 0.0, 0.0],
-    #         ]
-    #     ),
-    # }
+    dict_trajectory = {
+        "degrees": [2],
+        "knot_vectors": [[0.0, 0.0, 0.0, 0.333, 0.666, 1.0, 1.0, 1.0]],
+        "control_points": np.array(
+            [
+                [0.0, 0.0, 0.0],
+                [0.0, 0.0, 5.0],
+                [10.0, 5.0, 0.0],
+                [15.0, 0.0, -5.0],
+                [20.0, 0.0, 0.0],
+            ]
+        ),
+    }
 
     # 2D questionmark
     # dict_trajectory = {
@@ -46,78 +46,78 @@ if __name__ == "__main__":
     # }
     # init trajectory as bspline
 
-    # closed 3D questionmark
-    dict_trajectory = {
-        "degrees": [3],
-        "knot_vectors": [
-            [
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.1,
-                0.2,
-                0.3,
-                0.4,
-                0.5,
-                0.6,
-                0.8,
-                0.9,
-                1.0,
-                1.0,
-                1.0,
-                1.0,
-            ]
-        ],
-        "control_points": np.array(
-            [
-                [0.5, 0, 0],
-                [0.5, 2, 0.3],
-                [1.0, 3, 0.1],
-                [2.0, 4, -0.1],
-                [2.15, 5, -0.2],
-                [1.8, 5.9, -0.4],
-                [1.0, 6.2, -0.3],
-                [-0.25, 6, -0.1],
-                [-0.5, 5.0, 0.1],
-                [-2.0, 4.0, 0.2],
-                [-1, 3.0, 0.1],
-                [0.5, 0.0, 0.0],
-            ]
-        ),
-    }
+    # # closed 3D questionmark
+    # dict_trajectory = {
+    #     "degrees": [3],
+    #     "knot_vectors": [
+    #         [
+    #             0.0,
+    #             0.0,
+    #             0.0,
+    #             0.0,
+    #             0.1,
+    #             0.2,
+    #             0.3,
+    #             0.4,
+    #             0.5,
+    #             0.6,
+    #             0.8,
+    #             0.9,
+    #             1.0,
+    #             1.0,
+    #             1.0,
+    #             1.0,
+    #         ]
+    #     ],
+    #     "control_points": np.array(
+    #         [
+    #             [0.5, 0, 0],
+    #             [0.5, 2, 0.3],
+    #             [1.0, 3, 0.1],
+    #             [2.0, 4, -0.1],
+    #             [2.15, 5, -0.2],
+    #             [1.8, 5.9, -0.4],
+    #             [1.0, 6.2, -0.3],
+    #             [-0.25, 6, -0.1],
+    #             [-0.5, 5.0, 0.1],
+    #             [-2.0, 4.0, 0.2],
+    #             [-1, 3.0, 0.1],
+    #             [0.5, 0.0, 0.0],
+    #         ]
+    #     ),
+    # }
     trajectory = splinepy.BSpline(**dict_trajectory)
 
     # alternatively, use helpme to create a trajectory
     # trajectory = splinepy.helpme.create.circle(10)
 
     # insert knots and control points
-    trajectory.uniform_refine(0, 1)
+    trajectory.uniform_refine(0, 3)
 
     ### CROSS SECTION ###
-    # dict_cross_section = {
-    #     "degrees": [3],
-    #     "knot_vectors": [[0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0]],
-    #     "control_points": np.array(
-    #         [
-    #             [0.0, 0.0, 0.0],
-    #             [1.0, 2.0, 0.0],
-    #             [2.0, 0.0, 0.0],
-    #             [3.0, -2.0, 0.0],
-    #             [4.0, 0.0, 0.0],
-    #         ]
-    #     ),
-    # }
+    dict_cross_section = {
+        "degrees": [3],
+        "knot_vectors": [[0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0]],
+        "control_points": np.array(
+            [
+                [0.0, 0.0, 0.0],
+                [1.0, 2.0, 0.0],
+                [2.0, 0.0, 0.0],
+                [3.0, -2.0, 0.0],
+                [4.0, 0.0, 0.0],
+            ]
+        ),
+    }
 
-    # # init cross section as bspline
-    # cross_section = splinepy.BSpline(**dict_cross_section)
+    # init cross section as bspline
+    cross_section = splinepy.BSpline(**dict_cross_section)
 
     # alternatively, use helpme to create a cross section
-    cross_section = splinepy.helpme.create.surface_circle(0.5).nurbs
+    # cross_section = splinepy.helpme.create.surface_circle(0.5).nurbs
 
     # user can  define the normal vector of the cross section, in case
     # the cross section is not planar in the x-y plane (default)
-    cs_nv = np.array([0, 0, 1])
+    cs_nv = np.array([0, 1, 1])
 
     ### SWEEP ###
     swept_surface = splinepy.helpme.create.swept(
@@ -125,7 +125,7 @@ if __name__ == "__main__":
         cross_section=cross_section,
         cross_section_normal=cs_nv,
         auto_refinement=True,
-        set_on_trajectory=True,
+        set_on_trajectory=False,
     )
 
     ### VISUALIZATION ###
