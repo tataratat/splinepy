c7842f93519e0acc6e81db5a0c7dd393f45b5be0
markriegler <mark.riegler40@gmail.com>
2024-08-05 19:04:59 +0200

    Add inhomogeneous Dirichlet BCs from function

--- examples/iga/galerkin_laplace_problem_field_integrator.py
+++ examples/iga/galerkin_laplace_problem_field_integrator.py
@@ -1,6 +1,12 @@
 r"""
-This is a showcase of splinepy's FieldIntegrator class and is doing the same as
-'examples/galerkin_laplace_problem.py'.
+This is a showcase of splinepy's FieldIntegrator class. It solves the Poisson
+equation in the form
+.. math::
+        -\Delta u = f
+with source term f=1.
+
+First, homogeneous Dirichlet boundary conditions are applied. Then, inhomogeneous ones
+are applied.
 """
 
 import numpy as np
@@ -9,6 +15,7 @@ import splinepy as sp
 
 # Test Case
 n_refine = 15
+
 
 def prepare_geometry_and_solution_field():
     # Define the Geometry
@@ -27,21 +34,21 @@ def prepare_geometry_and_solution_field():
         ],
         knot_vectors=[[0, 0, 0, 1, 1, 1], [0, 0, 0, 1, 1, 1]],
     )
-    
+
     # Define the solution field
-    solution_field = sp.BSpline(
-        degrees=geometry.degrees,
-        control_points=np.ones((geometry.control_points.shape[0], 1)),
-        knot_vectors=geometry.knot_vectors,
+    solution_field = geometry.copy()
+    solution_field.control_points = np.ones(
+        (geometry.control_points.shape[0], 1)
     )
-    
+
     # Use refinement
     solution_field.elevate_degrees([0, 1, 0, 1])
     new_knots = np.linspace(1 / n_refine, 1, n_refine, endpoint=False)
     solution_field.insert_knots(0, new_knots)
     solution_field.insert_knots(1, new_knots)
-    
+
     return geometry, solution_field
+
 
 def poisson_lhs(mapper, quad_points, quad_weights, jacobian_det):
     bf_gradient, _ = mapper.basis_gradient_and_support(quad_points)
@@ -55,33 +62,47 @@ def poisson_lhs(mapper, quad_points, quad_weights, jacobian_det):
     )
     return element_matrix.ravel()
 
-def poisson_rhs(solution_field, quad_points, quad_weights, jacobian_det, current_sol=None):
-    bf, _ = solution_field.basis_and_support(quad_points)
+
+def poisson_rhs(mapper, quad_points, quad_weights, jacobian_det):
+    bf = mapper._field_reference.basis(quad_points)
     element_vector = np.einsum(
-        "qj,q,q->j",
-        bf,
-        quad_weights,
-        jacobian_det,
-        optimize=True
+        "qj,q,q->j", bf, quad_weights, jacobian_det, optimize=True
     )
     return element_vector
 
-if __name__ == "__main__":
-    geometry, solution_field = prepare_geometry_and_solution_field()
-    
-    fi = sp.helpme.integrate.FieldIntegrator(geometry=geometry, solution_field=solution_field)
-    
-    fi.assemble_matrix(poisson_lhs)
-    fi.assemble_vector(poisson_rhs)
-    fi.assign_homogeneous_dirichlet_boundary_conditions()
-    
-    fi.solve_linear_system()
-    
-    # Plot geometry and field
+
+def show_solution(geometry, solution_field):
     geometry.spline_data["field"] = solution_field
     geometry.show_options["data"] = "field"
     geometry.show_options["cmap"] = "jet"
     geometry.show_options["lighting"] = "off"
     geometry.show_options["scalarbar"] = True
     geometry.show(knots=True, control_points=False)
-    
+
+
+if __name__ == "__main__":
+    geometry, solution_field = prepare_geometry_and_solution_field()
+
+    fi = sp.helpme.integrate.FieldIntegrator(
+        geometry=geometry, solution_field=solution_field
+    )
+
+    fi.assemble_matrix(poisson_lhs)
+    fi.assemble_vector(poisson_rhs)
+
+    # Homogeneous Dirichlet boundary conditions
+    fi.assign_homogeneous_dirichlet_boundary_conditions()
+    fi.solve_linear_system()
+    # Plot geometry and field
+    show_solution(geometry, solution_field)
+
+    # Inhomogeneous Dirichlet boundary conditions
+    def dirichlet_function(points):
+        """
+        On the boundary apply: g(x,y) = x/10
+        """
+        return points[:, 0] / 10
+
+    fi.apply_dirichlet_boundary_conditions(dirichlet_function)
+    fi.solve_linear_system()
+    show_solution(geometry, solution_field)
