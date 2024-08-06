9653cd949fa1c5aa337c7068db79d659850ac05c
markriegler <mark.riegler40@gmail.com>
2024-08-06 17:34:40 +0200

    Put gismo additional blocks into separate class

--- examples/export_gismo.py
+++ examples/export_gismo.py
@@ -8,104 +8,131 @@ from splinepy.io import gismo
 
 EPS = 1e-8
 
-# Create multipatch geometry
-arc_inner = spp.NURBS(
-    degrees=[1, 2],
-    knot_vectors=[[0.0, 0.0, 1.0, 1.0], [0.0, 0.0, 0.0, 1.0, 1.0, 1.0]],
-    control_points=[
-        [1.0, 0.0],
-        [2.0, 0.0],
-        [1.0, 1.0],
-        [2.0, 2.0],
-        [0.0, 1.0],
-        [0.0, 2.0],
-    ],
-    weights=[1.0, 1.0, 0.707106781186548, 0.707106781186548, 1.0, 1.0],
-)
 
-arc_outer = arc_inner.copy()
-arc_outer.control_points = [
-    [2.0, 0.0],
-    [3.0, 0.0],
-    [2.0, 2.0],
-    [3.0, 3.0],
-    [0.0, 2.0],
-    [0.0, 3.0],
-]
+def create_geometry():
+    """Recreate multipatch geometry from g+smo repository
 
-multipatch = spp.Multipatch(splines=[arc_inner, arc_outer])
-multipatch.determine_interfaces()
-
-
-# Identifies the patches' boundaries which correspond to the Dirichlet BCs
-def dirichlet_identifier(points):
-    return (points[:, 1] < EPS) | (
-        (points[:, 0] < EPS) & (points[:, 1] > 2 - EPS)
+    Returns
+    ---------
+    multipatch: splinepy Multipatch
+        Arc geometry consisting of two patches
+    """
+    # Create inner and outer arcs
+    arc_inner = spp.NURBS(
+        degrees=[1, 2],
+        knot_vectors=[[0.0, 0.0, 1.0, 1.0], [0.0, 0.0, 0.0, 1.0, 1.0, 1.0]],
+        control_points=[
+            [1.0, 0.0],
+            [2.0, 0.0],
+            [1.0, 1.0],
+            [2.0, 2.0],
+            [0.0, 1.0],
+            [0.0, 2.0],
+        ],
+        weights=[1.0, 1.0, 0.707106781186548, 0.707106781186548, 1.0, 1.0],
     )
 
+    arc_outer = arc_inner.copy()
+    arc_outer.control_points = [
+        [2.0, 0.0],
+        [3.0, 0.0],
+        [2.0, 2.0],
+        [3.0, 3.0],
+        [0.0, 2.0],
+        [0.0, 3.0],
+    ]
 
-# Set Dirichlet boundaries to "BID2", Neumann BCs correspond to "BID1"
-multipatch.boundary_from_function(dirichlet_identifier, boundary_id=2)
+    # Initialize Multipatch object
+    multipatch = spp.Multipatch(splines=[arc_inner, arc_outer])
+    multipatch.determine_interfaces()
 
-# Create function block for rhs
-rhs = gismo.create_function_block(
-    dim=2,
-    function_id=1,
-    function_string="2*pi^2*sin(pi*x)*sin(pi*y)",
-    comment="Right-hand side function",
-)
+    # Identify the patches' boundaries which correspond to the Dirichlet BCs
+    def dirichlet_identifier(points):
+        return (points[:, 1] < EPS) | (
+            (points[:, 0] < EPS) & (points[:, 1] > 2 - EPS)
+        )
 
-# Create function block for manufactured solution
-manufactured_solution = gismo.create_function_block(
-    dim=2,
-    function_id=3,
-    function_string="sin(pi*x) * sin(pi*y)",
-    comment="The manufactured exact solution (for reference)",
-)
+    # Set Dirichlet boundaries to "BID2", Neumann BCs correspond to "BID1"
+    multipatch.boundary_from_function(dirichlet_identifier, boundary_id=2)
 
-# Create block for boundary conditions
-boundary_conditions = gismo.create_boundary_conditions_block(
-    bc_id=2,
-    dim=2,
-    function_list=[
-        "sin(pi*x) * sin(pi*y)",
-        ("pi*cos(pi*x) * sin(pi*y)", "pi*sin(pi*x) * cos(pi*y)"),
-        "0",
-    ],
-    bc_list=[("BID2", "Dirichlet", 0), ("BID1", "Neumann", 1)],
-    unknown_id=0,
-    multipatch_id=0,
-    comment="The boundary conditions (multipatch=number of patches)",
-)
+    return multipatch
 
-# Create dictionary for assembly options
-assembly_options = gismo.create_assembly_options_block(
-    options_id=4, comment="Assembler options"
-)
 
-# Visualize geometry and BCs
-boundary_names = ["Neumann boundary", "Dirichlet boundary"]
-spp.show(
-    ["Multipatch", multipatch],
-    *[
-        [
-            f"BID{i+1}: {boundary_names[i]}",
-            multipatch.boundary_multipatch(i + 1),
-        ]
-        for i in range(len(multipatch.boundaries))
-    ],
-    control_points=False,
-)
+def generate_additional_xml_blocks():
+    """
+    Create blocks for rhs, manufactured solution, boundary conditions and assembly
+    options.
 
-# Export to xml-file
-gismo.export(
-    fname="poisson2d_bvp_recreation.xml",
-    multipatch=multipatch,
-    indent=True,
-    options=[
-        rhs,
-        boundary_conditions,
-        manufactured_solution,
-        assembly_options,
-    ],
-)
+    Returns
+    -----------
+    additional_blocks: list<dict>
+        List of dictionaries used for splinepy's gismo export function
+    """
+
+    additional_blocks = gismo.AdditionalBlocks()
+
+    # Create function block for rhs
+    additional_blocks.add_function_block(
+        dim=2,
+        block_id=1,
+        function_string="2*pi^2*sin(pi*x)*sin(pi*y)",
+        comment="Right-hand side function",
+    )
+
+    # Create function block for manufactured solution
+    additional_blocks.add_function_block(
+        dim=2,
+        block_id=3,
+        function_string="sin(pi*x) * sin(pi*y)",
+        comment="The manufactured exact solution (for reference)",
+    )
+
+    # Create block for boundary conditions
+    additional_blocks.add_boundary_conditions_block(
+        block_id=2,
+        dim=2,
+        function_list=[
+            "sin(pi*x) * sin(pi*y)",
+            ("pi*cos(pi*x) * sin(pi*y)", "pi*sin(pi*x) * cos(pi*y)"),
+            "0",
+        ],
+        bc_list=[("BID2", "Dirichlet", 0), ("BID1", "Neumann", 1)],
+        unknown_id=0,
+        multipatch_id=0,
+        comment="The boundary conditions (multipatch=number of patches)",
+    )
+
+    # Create dictionary for assembly options
+    additional_blocks.add_assembly_options_block(
+        block_id=4, comment="Assembler options"
+    )
+
+    return additional_blocks._blocks
+
+
+if __name__ == "__main__":
+    multipatch = create_geometry()
+
+    additional_blocks = generate_additional_xml_blocks()
+
+    # Visualize geometry and BCs
+    boundary_names = ["Neumann boundary", "Dirichlet boundary"]
+    spp.show(
+        ["Multipatch", multipatch],
+        *[
+            [
+                f"BID{i+1}: {boundary_names[i]}",
+                multipatch.boundary_multipatch(i + 1),
+            ]
+            for i in range(len(multipatch.boundaries))
+        ],
+        control_points=False,
+    )
+
+    # Export to xml-file
+    gismo.export(
+        fname="poisson2d_bvp_recreation.xml",
+        multipatch=multipatch,
+        indent=True,
+        options=additional_blocks,
+    )
