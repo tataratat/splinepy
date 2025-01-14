ee0860028e679760b567151a68f0d0e10dd3d292
markriegler <mark.riegler40@gmail.com>
2025-01-14 10:46:10 +0100

    Add pytest parametrization of all the microtiles

--- tests/test_microstructure.py
+++ tests/test_microstructure.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pytest import mark
 
 import splinepy.microstructure as ms
 from splinepy.helpme.create import box
@@ -14,7 +15,8 @@ EPS = 1e-7
 CLOSURE_FAILS = [ms.tiles.HollowOctagonExtrude, ms.tiles.InverseCross3D]
 
 
-def test_closing_face():
+@mark.parametrize("tile_class", all_tile_classes)
+def test_closing_face(tile_class):
     """Check if closing face is working"""
 
     def check_if_closed(multipatch, closure_direction):
@@ -39,6 +41,7 @@ def test_closing_face():
                 > BOX_DIMENSIONS[direction_index] - EPS
             )
 
+        # create min- and max-boundaries of the multipatch using the identifiers
         multipatch.boundary_from_function(min_identifier, boundary_id=2)
         multipatch.boundary_from_function(max_identifier, boundary_id=3)
 
@@ -56,32 +59,31 @@ def test_closing_face():
             face_max_area > 1.0 - EPS
         ), f"The closure of the {closure_direction}_max surface is not complete"
 
-    for tile_class in all_tile_classes:
-        # Skip tile if it doesn't support closure
-        if "_closure_directions" not in dir(tile_class):
-            continue
+    # Skip tile if it doesn't support closure
+    if "_closure_directions" not in dir(tile_class):
+        return
 
-        # TODO: right now skip tiles which have faulty closures
-        if tile_class in CLOSURE_FAILS:
-            continue
+    # TODO: right now skip tiles which have faulty closures
+    if tile_class in CLOSURE_FAILS:
+        return
 
-        tile_creator = tile_class()
-        generator = ms.microstructure.Microstructure(
-            deformation_function=box(*BOX_DIMENSIONS[: tile_creator._dim]),
-            microtile=tile_creator,
-            tiling=TILING[: tile_creator._dim],
-        )
-        # Go through all implemented closure direction
-        closure_directions = {
-            directionname[0]
-            for directionname in tile_creator._closure_directions
-        }
-        for closure_direction in closure_directions:
-            multipatch = generator.create(closing_face=closure_direction)
-            check_if_closed(multipatch, closure_direction)
+    tile_creator = tile_class()
+    generator = ms.microstructure.Microstructure(
+        deformation_function=box(*BOX_DIMENSIONS[: tile_creator._dim]),
+        microtile=tile_creator,
+        tiling=TILING[: tile_creator._dim],
+    )
+    # Go through all implemented closure direction
+    closure_directions = {
+        directionname[0] for directionname in tile_creator._closure_directions
+    }
+    for closure_direction in closure_directions:
+        multipatch = generator.create(closing_face=closure_direction)
+        check_if_closed(multipatch, closure_direction)
 
 
-def test_macro_sensitivities(np_rng, heps=1e-7, n_test_points=10):
+@mark.parametrize("tile_class", all_tile_classes)
+def test_macro_sensitivities(tile_class, np_rng, heps=1e-7, n_test_points=10):
     """Testing the correctness of the derivatives of the whole microstructure w.r.t.
     the deformation function's control points. It is tested by evaluating the derivative
     obtained via finite differences. The values are evaluated at random points.
@@ -93,59 +95,58 @@ def test_macro_sensitivities(np_rng, heps=1e-7, n_test_points=10):
     n_test_points: int
         Number of testing points int the parametric domain"""
 
-    for tile_class in all_tile_classes:
-        tile_creator = tile_class()
-        deformation_function_orig = box(*BOX_DIMENSIONS[: tile_creator._dim])
-        generator = ms.microstructure.Microstructure(
-            deformation_function=deformation_function_orig,
-            microtile=tile_creator,
-            tiling=TILING[: tile_creator._dim],
-        )
-        multipatch = generator.create(macro_sensitivities=True)
-        dim = multipatch.dim
-        n_cps = deformation_function_orig.cps.shape[0]
+    tile_creator = tile_class()
+    deformation_function_orig = box(*BOX_DIMENSIONS[: tile_creator._dim])
+    generator = ms.microstructure.Microstructure(
+        deformation_function=deformation_function_orig,
+        microtile=tile_creator,
+        tiling=TILING[: tile_creator._dim],
+    )
+    multipatch = generator.create(macro_sensitivities=True)
+    dim = multipatch.dim
+    n_cps = deformation_function_orig.cps.shape[0]
 
-        # Set evaluation points as random spots in the parametric space
-        eval_points = np_rng.random((n_test_points, tile_creator._para_dim))
-        microstructure_orig_evaluations = [
-            patch.evaluate(eval_points) for patch in multipatch.patches
+    # Set evaluation points as random spots in the parametric space
+    eval_points = np_rng.random((n_test_points, tile_creator._para_dim))
+    microstructure_orig_evaluations = [
+        patch.evaluate(eval_points) for patch in multipatch.patches
+    ]
+    n_patches = len(multipatch.patches)
+
+    # Go through derivatives of every deformation function's control point
+    for ii_ctps in range(n_cps):
+        # Gradient through finite differences
+        deformation_function_perturbed = deformation_function_orig.copy()
+        deformation_function_perturbed.cps[ii_ctps, :] += heps
+        generator.deformation_function = deformation_function_perturbed
+        multipatch_perturbed = generator.create()
+        microstructure_perturbed_evaluations = [
+            patch.evaluate(eval_points)
+            for patch in multipatch_perturbed.patches
         ]
-        n_patches = len(multipatch.patches)
+        # Evaluate finite difference gradient
+        fd_sensitivity = [
+            (patch_perturbed - patch_orig) / heps
+            for patch_perturbed, patch_orig in zip(
+                microstructure_orig_evaluations,
+                microstructure_perturbed_evaluations,
+            )
+        ]
 
-        # Go through derivatives of every deformation function's control point
-        for ii_ctps in range(n_cps):
-            # Gradient through finite differences
-            deformation_function_perturbed = deformation_function_orig.copy()
-            deformation_function_perturbed.cps[ii_ctps, :] += heps
-            generator.deformation_function = deformation_function_perturbed
-            multipatch_perturbed = generator.create()
-            microstructure_perturbed_evaluations = [
-                patch.evaluate(eval_points)
-                for patch in multipatch_perturbed.patches
+        # Go through each direction
+        for jj_dim in range(dim):
+            deriv_orig = multipatch.fields[ii_ctps * dim + jj_dim]
+            deriv_evaluations = [
+                patch.evaluate(eval_points) for patch in deriv_orig.patches
             ]
-            # Evaluate finite difference gradient
-            fd_sensitivity = [
-                (patch_perturbed - patch_orig) / heps
-                for patch_perturbed, patch_orig in zip(
-                    microstructure_orig_evaluations,
-                    microstructure_perturbed_evaluations,
+            for k_patch, patch_deriv_implemented, patch_deriv_fd in zip(
+                range(n_patches), deriv_evaluations, fd_sensitivity
+            ):
+                assert np.allclose(
+                    -patch_deriv_implemented[:, jj_dim],
+                    patch_deriv_fd[:, jj_dim],
+                ), (
+                    "Implemented derivative calculation for tile class"
+                    + f"{tile_class}, at patch {k_patch+1}/{n_patches} does not "
+                    + "match the derivative obtained using Finite Differences"
                 )
-            ]
-
-            # Go through each direction
-            for jj_dim in range(dim):
-                deriv_orig = multipatch.fields[ii_ctps * dim + jj_dim]
-                deriv_evaluations = [
-                    patch.evaluate(eval_points) for patch in deriv_orig.patches
-                ]
-                for k_patch, patch_deriv_implemented, patch_deriv_fd in zip(
-                    range(n_patches), deriv_evaluations, fd_sensitivity
-                ):
-                    assert np.allclose(
-                        -patch_deriv_implemented[:, jj_dim],
-                        patch_deriv_fd[:, jj_dim],
-                    ), (
-                        "Implemented derivative calculation for tile class"
-                        + f"{tile_class}, at patch {k_patch+1}/{n_patches} does not "
-                        + "match the derivative obtained using Finite Differences"
-                    )
