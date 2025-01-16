cbdf2464100dc461bfe693685f8ee8e01ecd5150
markriegler <mark.riegler40@gmail.com>
2025-01-16 15:22:39 +0100

    Refactor, add common code to TileBase

--- splinepy/microstructure/tiles/tile_base.py
+++ splinepy/microstructure/tiles/tile_base.py
@@ -12,6 +12,11 @@ class TileBase(_SplinepyBase):
     _para_dim = None
     _evaluation_points = None
     _n_info_per_eval_point = None
+    _sensitivities_implemented = None
+    _closure_directions = None
+    _parameter_bounds = None
+    _parameters_shape = None
+    _default_parameter_value = None
 
     def __init__(self):
         if type(self) is TileBase:
@@ -46,6 +51,20 @@ class TileBase(_SplinepyBase):
         return cls._raise_if_not_set_else_return("_evaluation_points")
 
     @property
+    def n_info_per_eval_point(cls):
+        """Number of parameters per evaluation point
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        n_info : int
+        """
+        return cls._raise_if_not_set_else_return("_n_info_per_eval_point")
+
+    @property
     def dim(cls):
         """Returns dimensionality in physical space of the Microtile.
 
@@ -73,8 +92,79 @@ class TileBase(_SplinepyBase):
         """
         return cls._raise_if_not_set_else_return("_para_dim")
 
-    def check_params(self, params):
-        """Checks if the parameters have the correct format and shape
+    @property
+    def sensitivites_implemented(cls):
+        """Returns whether sensitivities are implemented for the microtile
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        is_implemented: bool
+        """
+        return cls._raise_if_not_set_else_return("_sensitivities_implemented")
+
+    @property
+    def closure_directions(self):
+        """Returns the available closure directions of the microtile
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        directions: None/list<str>
+        """
+        return self._closure_directions
+
+    @property
+    def parameter_bounds(cls):
+        """Returns the bounds for the microtiles' parameters
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        bounds: list<list<float>>
+        """
+        return cls._raise_if_not_set_else_return("_parameter_bounds")
+
+    @property
+    def parameters_shape(cls):
+        """Returns the shape of the microtile's parameters array
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        shape: tuple<int>
+        """
+        return cls._raise_if_not_set_else_return("_parameters_shape")
+
+    @property
+    def default_parameter_value(cls):
+        """Returns the default value of the microtile's parameters
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        default_value: float/None
+        """
+        return cls._default_parameter_value
+
+    def check_params(self, parameters):
+        """Checks if the parameters have the correct format and shape and are within
+        defined bounds
 
         Parameters
         ----------
@@ -85,20 +175,34 @@ class TileBase(_SplinepyBase):
         -------
         True: Boolean
         """
-        # check if tuple
-
-        if not (isinstance(params, _np.ndarray) and params.ndim == 2):
+        # Check correct format
+        if not (isinstance(parameters, _np.ndarray) and parameters.ndim == 2):
             raise TypeError("parameters must be two-dimensional np array")
 
+        # Check correct shape
         if not (
-            (self._evaluation_points.shape[0] == params.shape[0])
-            and (self._n_info_per_eval_point == params.shape[1])
+            (self._evaluation_points.shape[0] == parameters.shape[0])
+            and (self._n_info_per_eval_point == parameters.shape[1])
         ):
             raise TypeError(
                 f"Mismatch in parameter size, expected "
                 f"{self._evaluation_points.shape[0]} x "
                 f"{self._n_info_per_eval_point}"
             )
+
+        # Check if all parameters are within bounds
+        if self._parameter_bounds is not None:
+            bounds = _np.array(self._parameter_bounds)
+            lower_bounds = bounds[:, 0]
+            upper_bounds = bounds[:, 1]
+            within_bounds = (parameters.ravel() > lower_bounds) & (
+                parameters.ravel() < upper_bounds
+            )
+            if not _np.all(within_bounds):
+                raise ValueError(
+                    f"The parameters {parameters} must be within the following bounds: "
+                    + f"lower: {lower_bounds} and upper: {upper_bounds}"
+                )
 
         return True
 
@@ -153,3 +257,72 @@ class TileBase(_SplinepyBase):
         raise NotImplementedError(
             f"create_tile() not implemented for {type(self)}"
         )
+
+    def _process_input(self, parameters, parameter_sensitivities):
+        """Processing input for create_tile and _closing_tile
+
+        Parameters
+        -------------
+        parameters: np.ndarray
+            Tile parameters
+        parameter_sensitivities: np.ndarray
+            Tile parameter sensitivities to be calculated
+
+        Returns
+        ---------
+        parameters: np.ndarray
+            Tile parameters
+        n_derivatives: np.ndarray
+            Number of different derivatives to compute
+        derivatives: list/None
+            Initialized list of derivatives
+        """
+        # Set parameters to default values if not user-given
+        if parameters is None:
+            default_value = self.default_parameter_value
+            self._logd(
+                f"Setting parameters to default values ({default_value})"
+            )
+            if isinstance(default_value, float):
+                parameters = _np.full(
+                    (len(self.evaluation_points), self.n_info_per_eval_point),
+                    default_value,
+                )
+            elif isinstance(default_value, _np.ndarray):
+                parameters = default_value
+
+        # Initialize list of derivatives
+        if parameter_sensitivities is not None:
+            n_derivatives = parameter_sensitivities.shape[2]
+            derivatives = []
+        else:
+            n_derivatives = 0
+            derivatives = None
+
+        # Validity check of parameters and their sensitivities
+        self.check_params(parameters)
+        self.check_param_derivatives(parameter_sensitivities)
+
+        return parameters, n_derivatives, derivatives
+
+    def _check_custom_parameter(self, value, param_name, min_bound, max_bound):
+        """Check if a custom tile parameter (e.g. contact length) is within bounds
+
+        Parameters
+        ------------------
+        value: float
+            Value of the custom parameter
+        param_name: str
+            Name of custom parameter
+        min_bound: float
+            Lower bound for parameter
+        max_bound: float
+            Upper bound for parameter
+        """
+        if not isinstance(value, float):
+            raise ValueError(f"Invalid type for {param_name}")
+
+        if not ((value > min_bound) and (value < max_bound)):
+            raise ValueError(
+                f"{param_name} must be in ({min_bound}, {max_bound})"
+            )
