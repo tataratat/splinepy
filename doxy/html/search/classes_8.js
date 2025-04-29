b6c7e5788d04ea00b5a22fb072a9c4d2fa7b21bf
clemens-fricke <clemens-fricke@users.noreply.github.com>
2025-04-29 14:09:32 +0000

    deploy: b6e70631b549ee889b234e0597de4c4ea0622003

---
+++ doxy/html/search/classes_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['proximity_0',['Proximity',['../classsplinepy_1_1proximity_1_1Proximity.html',1,'splinepy::proximity']]],
+  ['pymultipatch_1',['PyMultipatch',['../classsplinepy_1_1py_1_1PyMultipatch.html',1,'splinepy::py']]],
+  ['pyspline_2',['PySpline',['../classsplinepy_1_1py_1_1PySpline.html',1,'splinepy::py']]]
+];
---
+++ doxy/html/search/classes_a.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['searchdata_0',['SearchData',['../structsplinepy_1_1proximity_1_1SearchData.html',1,'splinepy::proximity']]],
+  ['slsqpdata_1',['SLSQPData',['../structsplinepy_1_1proximity_1_1SearchData_1_1SLSQPData.html',1,'splinepy::proximity::SearchData']]],
+  ['splinepybase_2',['SplinepyBase',['../classsplinepy_1_1splines_1_1SplinepyBase.html',1,'splinepy::splines']]]
+];
