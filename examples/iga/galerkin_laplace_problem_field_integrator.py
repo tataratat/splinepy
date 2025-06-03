81bcbdb1f548e90d28372b5efe8e7a721a1cc2a2
markriegler <mark.riegler40@gmail.com>
2025-06-03 10:52:54 +0200

    Add comments for more clarity

--- examples/iga/galerkin_laplace_problem_field_integrator.py
+++ examples/iga/galerkin_laplace_problem_field_integrator.py
@@ -5,8 +5,9 @@ equation in the form
         -\Delta u = f
 with source term f=1.
 
-First, homogeneous Dirichlet boundary conditions are applied. Then, homogeneous
-and inhomogeneous Dirichlet boundary conditions are applied on 3 of 4 boundaries.
+First, homogeneous Dirichlet boundary conditions are applied. Then, a mix of homogeneous
+and inhomogeneous Dirichlet boundary conditions are applied on 3 of 4 boundaries. A
+zero Neumann boundary condition is implicitly applied on the fourth boundary.
 """
 
 import numpy as np
@@ -18,6 +19,17 @@ n_refine = 15
 
 
 def prepare_geometry_and_solution_field():
+    """
+    Creates single patch geometry and solution field with h- and p-refinement
+
+    Returns
+    -----------------
+    geometry: splinepy.BSpline
+        The geometry object
+    solution_field: splinepy.BSpline
+        The solution field with refinements. Its control points serve as the degree of
+        freedoms (DoFs) of the solution. It is initialized to a vector of ones.
+    """
     # Define the Geometry
     geometry = sp.BSpline(
         degrees=[2, 2],
@@ -37,12 +49,14 @@ def prepare_geometry_and_solution_field():
 
     # Define the solution field
     solution_field = geometry.copy()
+    # Initialize solution vector
     solution_field.control_points = np.ones(
         (geometry.control_points.shape[0], 1)
     )
 
-    # Use refinement
+    # Apply p-refinement
     solution_field.elevate_degrees([0, 1, 0, 1])
+    # Apply uniform h-refinement
     new_knots = np.linspace(1 / n_refine, 1, n_refine, endpoint=False)
     solution_field.insert_knots(0, new_knots)
     solution_field.insert_knots(1, new_knots)
@@ -120,22 +134,26 @@ def show_solution(geometry, solution_field):
 
 
 if __name__ == "__main__":
+    # Create geometry and solution field with h- and p-refinement
     geometry, solution_field = prepare_geometry_and_solution_field()
 
     fi = sp.helpme.integrate.FieldIntegrator(
         geometry=geometry, solution_field=solution_field
     )
 
+    # Assemble the linear system for the Poisson equation
     fi.assemble_matrix(poisson_lhs)
     fi.assemble_vector(poisson_rhs)
 
-    # Homogeneous Dirichlet boundary conditions
+    # Apply homogeneous Dirichlet boundary conditions
     fi.apply_homogeneous_dirichlet_boundary_conditions()
+    # Solve linear system to obtain solution vector, which is stored as the
+    # solution_field's control points
     fi.solve_linear_system()
     # Plot geometry and field
     show_solution(geometry, solution_field)
 
-    # Inhomogeneous Dirichlet boundary conditions
+    # Function for inhomogeneous Dirichlet boundary conditions
     def dirichlet_function(points):
         """
         On the boundary apply: g(x,y) = x/4
@@ -146,10 +164,14 @@ if __name__ == "__main__":
     fi.assemble_matrix(poisson_lhs)
     fi.assemble_vector(poisson_rhs)
 
-    # Apply boundary conditions on 3 boundaries
+    # Apply boundary conditions on 3 boundaries (west, south and north boundary)
     fi.apply_homogeneous_dirichlet_boundary_conditions(west=True)
     fi.apply_dirichlet_boundary_conditions(
         dirichlet_function, south=True, north=True
     )
+    # For zero Neumann boundary conditions we can keep the matrix and rhs as they are
+
+    # Solve linear system
     fi.solve_linear_system()
+    # Plot resulting solution
     show_solution(geometry, solution_field)
