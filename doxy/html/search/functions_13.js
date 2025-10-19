60616a292e6d1c451c819c571c29fc70fe9e5a61
j042 <j042@users.noreply.github.com>
2025-10-19 11:04:12 +0000

    deploy: f70e0e637c1dace2166638554e571e79e4517149

---
+++ doxy/html/search/functions_13.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['uniqueindicesandmultiplicities_0',['UniqueIndicesAndMultiplicities',['../namespacesplinepy_1_1utils.html#a1d335ae1cfa4b71bb235ae1bcddf33d6',1,'splinepy::utils']]],
+  ['updateandclip_1',['UpdateAndClip',['../classsplinepy_1_1proximity_1_1Proximity.html#abbfc07f94c7158549508652caf8d9970',1,'splinepy::proximity::Proximity']]]
+];
---
+++ doxy/html/search/functions_4.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['elevatedegree_0',['elevatedegree',['../classsplinepy_1_1splines_1_1RationalBezier.html#a295928c8177dccebb36ad246b2af01af',1,'splinepy::splines::RationalBezier::ElevateDegree()'],['../classsplinepy_1_1splines_1_1Bezier.html#a295928c8177dccebb36ad246b2af01af',1,'splinepy::splines::Bezier::ElevateDegree()']]],
+  ['elevatedegrees_1',['ElevateDegrees',['../classsplinepy_1_1py_1_1PySpline.html#acaaf8a6ec687d936f5881779bdde5958',1,'splinepy::py::PySpline']]],
+  ['end_2',['end',['../classsplinepy_1_1utils_1_1Array.html#af84a72c8309221e4487f0089cefea1c7',1,'splinepy::utils::Array']]],
+  ['evaluate_3',['evaluate',['../classsplinepy_1_1py_1_1PyMultipatch.html#a313293920957b7ff24b6f116ffff2ff1',1,'splinepy::py::PyMultipatch::Evaluate()'],['../classsplinepy_1_1py_1_1PySpline.html#a6bc4484d5b815817d892cf9a4473c61a',1,'splinepy::py::PySpline::Evaluate()']]],
+  ['evaluateboundarycenters_4',['EvaluateBoundaryCenters',['../namespacesplinepy_1_1py.html#a7d93e0caa24b51355f12250993739bc7',1,'splinepy::py']]],
+  ['exportiges_5',['ExportIges',['../namespacesplinepy_1_1py.html#a8171653ebc75e667984e0b3c432bd559',1,'splinepy::py']]],
+  ['extractallboundarysplines_6',['ExtractAllBoundarySplines',['../namespacesplinepy_1_1py.html#a20f803f3d17344179ddd3499e71bf32d',1,'splinepy::py']]],
+  ['extractbezierpatches_7',['extractbezierpatches',['../namespacesplinepy_1_1py.html#a69ccd54675655ebe1388f11ca43ab7ab',1,'splinepy::py::ExtractBezierPatches()'],['../namespacesplinepy_1_1splines_1_1helpers.html#adaefa093328da4859a3430ceffcfc9db',1,'splinepy::splines::helpers::ExtractBezierPatches(const SplineType &amp;spline)']]],
+  ['extractbezierpatchids_8',['ExtractBezierPatchIDs',['../namespacesplinepy_1_1splines_1_1helpers.html#a2cdf12123838ad703b58e369f120b83f',1,'splinepy::splines::helpers']]],
+  ['extractboundaries_9',['ExtractBoundaries',['../namespacesplinepy_1_1py.html#aa59ae55bee4f8f3279e3d94fa068a90e',1,'splinepy::py']]],
+  ['extractboundaryfromaxisandextrema_10',['ExtractBoundaryFromAxisAndExtrema',['../namespacesplinepy_1_1splines_1_1helpers.html#ac6319f0a68ca229fdd1f75ed366eccf2',1,'splinepy::splines::helpers']]],
+  ['extractboundarymeshslice_11',['ExtractBoundaryMeshSlice',['../namespacesplinepy_1_1splines_1_1helpers.html#a8f7d70c32275230dd17b4479f8b5d320',1,'splinepy::splines::helpers']]],
+  ['extractcontrolmeshslice_12',['ExtractControlMeshSlice',['../namespacesplinepy_1_1splines_1_1helpers.html#abde64cb35991da416313105000f70c85',1,'splinepy::splines::helpers']]],
+  ['extractcontrolmeshslicefromids_13',['ExtractControlMeshSliceFromIDs',['../namespacesplinepy_1_1splines_1_1helpers.html#adedbea6623aea6743b2b49c336d71558',1,'splinepy::splines::helpers']]],
+  ['extractdim_14',['ExtractDim',['../namespacesplinepy_1_1py.html#a8b51ee28ed5db8cfcd067517bb52630f',1,'splinepy::py']]]
+];
