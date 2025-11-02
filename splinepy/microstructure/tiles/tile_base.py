bd9a4fa7098164469d3c69ca8c980e7452dd2656
jzwar <jacqueszwar@gmail.com>
2025-11-02 21:07:57 +0100

    Remove classmethods from tile base

--- splinepy/microstructure/tiles/tile_base.py
+++ splinepy/microstructure/tiles/tile_base.py
@@ -30,7 +30,6 @@ class TileBase(_SplinepyBase):
             )
         return attr
 
-    @classmethod
     @property
     def evaluation_points(cls):
         """Positions in the parametrization function to be evaluated when tile
@@ -46,9 +45,8 @@ class TileBase(_SplinepyBase):
         """
         return cls._raise_if_not_set_else_return("_evaluation_points")
 
-    @classmethod
     @property
-    def dim(cls):
+    def dim(self):
         """Returns dimensionality in physical space of the Microtile.
 
         Parameters
@@ -59,11 +57,10 @@ class TileBase(_SplinepyBase):
         -------
         dim : int
         """
-        return cls._raise_if_not_set_else_return("_dim")
+        return self._raise_if_not_set_else_return("_dim")
 
-    @classmethod
     @property
-    def para_dim(cls):
+    def para_dim(self):
         """Returns dimensionality in parametric space of the Microtile.
 
         Parameters
@@ -74,7 +71,7 @@ class TileBase(_SplinepyBase):
         -------
         para_dim : int
         """
-        return cls._raise_if_not_set_else_return("_para_dim")
+        return self._raise_if_not_set_else_return("_para_dim")
 
     def check_params(self, params):
         """Checks if the parameters have the correct format and shape
